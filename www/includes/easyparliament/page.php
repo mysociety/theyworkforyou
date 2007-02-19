8fabee6b1bcb48f2e5d390ac34f7c350cf789252
twfy-staging <twfy-staging>
2007-02-19 17:38:15 +0000

    Added NZ advert

--- www/includes/easyparliament/page.php
+++ www/includes/easyparliament/page.php
@@ -2,7 +2,7 @@
 
 if (defined('OPTION_TRACKING') && OPTION_TRACKING)
 	require_once INCLUDESPATH . '../../../phplib/tracking.php';
-#require_once INCLUDESPATH . '../../../phplib/gaze.php';
+include_once INCLUDESPATH . '../../../phplib/gaze.php';
 
 class PAGE {
 
@@ -244,6 +244,9 @@ if (typeof urchinTracker == 'function') urchinTracker();
 		twfy_debug ("PAGE", "This page: $this_page");
 		
 		print "\t<a name=\"top\"></a>\n\n";
+		if (gaze_get_country_from_ip($_SERVER["REMOTE_ADDR"]) == 'NZ' || get_http_var('nz')) {
+			print '<p align="center"><strong>New!</strong> You\'re in New Zealand, so check out <a href="http://www.theyworkforyou.co.nz">TheyWorkForYou.co.nz</a></p>';
+		}
 
 		$this->title_bar();
 		
