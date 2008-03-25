90a2c29339b892c9500f7eed3832df2188d941d7
francis <francis>
2008-03-25 14:06:58 +0000

    OK, only one platypus per page.

--- www/includes/easyparliament/page.php
+++ www/includes/easyparliament/page.php
@@ -662,14 +662,18 @@ if (typeof urchinTracker == 'function') urchinTracker();
 			</div> <!-- end .main -->
 			<div class="sidebar">
 
-        <? /* if ($this_page != "campaign") { ?>
+        <? 
+            global $already_done_platypus;
+            if ($this_page != "campaign" && !$already_done_platypus) { 
+                $already_done_platypus = 1;
+            ?>
 			<div class="block">
             <h4>Click on the platypus!</h4>
             <a href="/freeourbills">
             <img title="Duck-billed platypus" src="/freeourbills/bill3.jpg" alt="Free Our Bills!" hspace="10" vspace="10">
             </a>
             </div>
-        <? } */ ?>
+        <? } ?>
 <?php
 		$this->within_stripe_sidebar = true;
 		$extrahtml = '';
