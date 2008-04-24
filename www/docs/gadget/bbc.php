f3c2a58bf9066e1dbf4ff179176736d89e5d15bf
matthew <matthew>
2008-04-24 09:33:30 +0000

    Add party site.

--- www/docs/gadget/bbc.php
+++ www/docs/gadget/bbc.php
@@ -7,6 +7,20 @@ include_once '../api/api_getMP.php';
 $db = new ParlDB;
 $_GET['output'] = 'xml';
 
+$party_sites = array(
+    'Conservative' => 'http://www.conservatives.com/',
+    'Democratic Unionist' => 'http://www.dup.org.uk/',
+    'Labour' => 'http://www.labour.org.uk/',
+    'Liberal Democrats' => 'http://www.libdems.org.uk/',
+    'Plaid Cymru' => 'http://www.plaidcymru.org/',
+    'Respect' => 'http://www.respectcoalition.org/',
+    'Social Democratic and Labour Party' => 'http://www.sdlp.ie/',
+    'Sinn Fein' => 'http://sinnfein.ie/',
+    'Scottish National Party' => 'http://www.snp.org/',
+    'UKIP' => 'http://www.ukip.org/',
+    'Ulster Unionist' => 'http://www.uup.org/',
+);
+
 $bbc_cons_id = get_http_var('id');
 if (!ctype_digit($bbc_cons_id) || !$bbc_cons_id)
 	error('Invalid constituency ID');
@@ -34,6 +48,15 @@ if ($action == 'latest') {
 	$output = _api_getMP_row($q->row(0));
 	foreach (array('house', 'first_name', 'last_name', 'title', 'person_id') as $key) {
 		unset($output[$key]);
+	}
+	if (isset($party_sites[$output['party']])) {
+		$output['party_site'] = $party_sites[$output['party']];
+	} elseif ($output['full_name'] == 'Richard Taylor') {
+    		$output['party_site'] = 'http://www.healthconcern.org.uk/';
+	} elseif ($output['full_name'] == 'Dai Davies') {
+		$output['party_site'] = 'http://www.blaenaugwentpeoplesvoice.org/';
+	} else {
+		$output['party_site'] = '';
 	}
 	if (isset($output['image'])) $output['image'] = 'http://www.theyworkforyou.com' . $output['image'];
 	$output['bbc'] = 'http://news.bbc.co.uk/1/shared/mpdb/html/' . $bbc_cons_id . '.stm';
