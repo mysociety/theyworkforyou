e099cb86b6588f9ea92712301a079fb35794c906
matthew <matthew>
2008-05-21 15:45:45 +0000

    alt attributes.

--- www/includes/easyparliament/page.php
+++ www/includes/easyparliament/page.php
@@ -299,7 +299,7 @@ if (typeof urchinTracker == 'function') urchinTracker();
 		}
 		?>
 	<div id="banner">
-		<a href="http://www.mysociety.org"><img id="logo" src="http://www.mysociety.org/mysociety_sm.gif"></a>
+		<a href="http://www.mysociety.org"><img id="logo" alt="Visit mySociety.org" src="http://www.mysociety.org/mysociety_sm.gif"></a>
 		<div id="title">
 			<h1><?php echo $img; ?></h1>
 		</div>
