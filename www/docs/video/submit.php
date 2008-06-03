1e850bb532e62045972c2d7cf31c2197e05568a9
matthew <matthew>
2008-06-03 16:37:40 +0000

    If a logged in user oops a timestamp and then redoes it, change deleted flag back to 0! Whoops :-/

--- www/docs/video/submit.php
+++ www/docs/video/submit.php
@@ -44,7 +44,7 @@ $new_time = date('H:i:s', $epoch);
 
 if ($THEUSER->isloggedin()) {
 	$user_id = $THEUSER->user_id();
-	$q = $db->query("insert into video_timestamps (gid, user_id, atime) values ('$q_gid', $user_id, '$new_time') on duplicate key update atime=VALUES(atime)");
+	$q = $db->query("insert into video_timestamps (gid, user_id, atime) values ('$q_gid', $user_id, '$new_time') on duplicate key update atime=VALUES(atime),deleted=0");
 } else {
 	$q = $db->query("insert into video_timestamps (gid, atime) values ('$q_gid', '$new_time')");
 }
