4da0a8e9f40704f9057b448cb2c73ddaba0258b8
matthew <matthew>
2008-06-03 11:04:21 +0000

    No need for extra distance step.

--- www/docs/video/distance.php
+++ www/docs/video/distance.php
@@ -8,28 +8,17 @@ $gid = get_http_var('gid');
 preg_match('#^(\d\d\d\d)-(\d\d)-(\d\d)#', $gid, $m);
 $gid_date = "$m[1]$m[2]$m[3]";
 
-global $gid_want, $file_want; # It's in the template, yucky, sorry
-$gid_want = $gid;
-$file_want = $file;
+global $want; # It's in the template, yucky, sorry
+$want['gid'] = $gid;
+$want['file'] = $file;
 
-if ($at = get_http_var('at')) {
-	$dist = speeches_distance($gid, $at);
-	echo '<p>You are watching a speech <strong>';
-	if ($dist > 0) {
-		echo $dist . ' speeches/headings ahead</strong> of the speech you want.';
-	} else {
-		echo (-$dist) . ' speeches/headings behind</strong> the speech you want.';
-	} 
-	echo ' <a target="_top" onclick="t = parent.document[\'video\'].currentTime(); this.href += t;" href="/video/?gid=' . $at . '&amp;file=' . $file . '&amp;start=">Match this speech instead</a></p>';
-
-	echo search_box($file);
-} elseif ($search = get_http_var('s')) {
+if ($search = get_http_var('s')) {
 	search_box($file);
 	day_speeches($search, $gid_date);
 } else { ?>
 <p>If the playing speech appears to be in completely the wrong place,
-enter the speaking MP's name or something they're saying in this search box
-to search the day's speeches. Pick the correct speech and we'll tell you how
+enter the speaking MP's name and/or something they're saying in this search box
+to search the day's speeches. Find the correct speech and it will say how
 many speeches out the video is &ndash; you can then either reposition the video
 or follow the link to switch to matching that speech instead.</p>
 <?
@@ -37,47 +26,28 @@ or follow the link to switch to matching that speech instead.</p>
 }
 
 function search_box($file) {
-	global $gid_want;
+	global $want;
 ?>
 <form action="distance.php" method="get" target="video_person_search">
 <label for="vid_search">MP name or spoken word(s):</label>
 <input id="vid_search" type="text" name="s" value="">
-<input type="hidden" name="gid" value="<?=$gid_want?>">
+<input type="hidden" name="gid" value="<?=$want['gid']?>">
 <input type="hidden" name="file" value="<?=$file?>">
 <input type="submit" value="Search">
 </form>
 <?
 }
 
-function speeches_distance($want, $at) {
-	$want = "uk.org.publicwhip/debate/$want";
-	$at = "uk.org.publicwhip/debate/$at";
-	$q_want = mysql_escape_string($want);
-	$q_at = mysql_escape_string($at);
-	$db = new ParlDB;
-	$q = $db->query("select gid, hdate, hpos from hansard where gid='$q_at' or gid='$q_want'");
-	for ($i=0; $i<$q->rows(); $i++) {
-		$r_gid = $q->field($i, 'gid');
-		$hdate = $q->field($i, 'hdate');
-		$hpos = $q->field($i, 'hpos');
-		if ($r_gid == $want) {
-			$want = array($r_gid, $hdate, $hpos);
-		} else {
-			$at = array($r_gid, $hdate, $hpos);
-		}
-	}
-
-	if ($want[1] != $at[1])
-		print 'ERROR: Different days';
-
-	return $at[2]-$want[2];
-}
-
 function day_speeches($search, $date) {
 	$search = "$search date:$date section:debates groupby:speech";
 
-	global $SEARCHENGINE;
+	global $SEARCHENGINE, $want;
 	$SEARCHENGINE = new SEARCHENGINE($search);
+
+	$db = new ParlDB;
+	$q_gid = mysql_escape_string('uk.org.publicwhip/debate/' . $want['gid']);
+	$q = $db->query("select hpos from hansard where gid='$q_gid'");
+	$want['hpos'] = $q->field(0, 'hpos');
 
 	$LIST = new DEBATELIST;
     	$args = array (
--- www/includes/easyparliament/templates/html/hansard_search_video.php
+++ www/includes/easyparliament/templates/html/hansard_search_video.php
@@ -3,11 +3,15 @@
  * For displaying day of MP stuff for matching against playing video
  */
  
-global $gid_want, $file_want; # XXX
+global $want; # XXX
 
 twfy_debug("TEMPLATE", "hansard_search_video.php");
 
 ?>
+
+<style type="text/css">
+.hi { background-color: #ffff33; }
+</style>
 
 <p style="font-size:larger;background-color:#ccccff;padding:6px;">Okay, here
 are the search results for that for the current day. Hopefully you recognise
@@ -29,7 +33,13 @@ if (isset ($data['rows']) && count($data['rows']) > 0) {
 		} 
 		
 		echo '&#8220;' . $row['body'] . "&#8221;</dd>\n";
-		echo '<dd><a href="distance.php?gid=' . $gid_want . '&amp;file=' . $file_want . '&amp;at=' . $row['gid'] . '">This speech is playing</a></dd>';
+		echo '<dd><em><small>This speech is ';
+		if ($row['hpos']-$want['hpos']>0) {
+			echo ($row['hpos']-$want['hpos']) . ' ahead';
+		} else {
+			echo ($want['hpos']-$row['hpos']) . ' behind';
+		}
+		echo ' of the one you want &ndash; if you prefer, <a target="_top" onclick="t = parent.document[\'video\'].currentTime(); this.href += t;" href="/video/?gid=' . $row['gid'] . '&amp;file=' . $want['file'] . '&amp;start=">switch to matching this speech instead</a></small></em></dd>';
 	}
 	echo '</dl>';
 } else {
