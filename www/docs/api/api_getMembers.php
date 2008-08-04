61f6217962907ee1991ea720df8ac74eda4de1ff
matthew <matthew>
2008-08-04 11:05:26 +0000

    MLAs/MSPs do have constituencies.

--- www/docs/api/api_getMembers.php
+++ www/docs/api/api_getMembers.php
@@ -17,7 +17,7 @@ function _api_getMembers_output($sql) {
 				$q->field($i, 'constituency') )),
 			'party' => isset($parties[$q->field($i, 'party')]) ? $parties[$q->field($i, 'party')] : $q->field($i, 'party'),
 		);
-		if ($q->field($i, 'house') == 1)
+		if ($q->field($i, 'house') != 2)
 			$row['constituency'] = html_entity_decode($q->field($i, 'constituency'));
 		$output[] = $row;
 		$time = strtotime($q->field($i, 'lastupdate'));
