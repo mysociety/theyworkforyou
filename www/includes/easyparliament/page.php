3c73eaa56ea682ed596b309a7f9e49e22a1e160d
francis <francis>
2008-08-27 10:22:20 +0000

    Add piwik

--- www/includes/easyparliament/page.php
+++ www/includes/easyparliament/page.php
@@ -929,6 +929,24 @@ pr()//-->
 		?>
 </div> <!-- end #container -->
 
+<? if (DOMAIN == 'www.theyworkforyou.com') { ?>
+<!-- Piwik -->
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.mysociety.org/" : "http://piwik.mysociety.org/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+<!--
+piwik_action_name = '';
+piwik_idsite = 7;
+piwik_url = pkBaseURL + "piwik.php";
+piwik_log(piwik_action_name, piwik_idsite, piwik_url);
+//-->
+</script>
+<noscript><img src="http://piwik.mysociety.org/piwik.php?i=1" style="border:0" alt=""></noscript>
+<!-- /Piwik -->
+<? } ?>
+
 </body>
 </html>
 <?php
