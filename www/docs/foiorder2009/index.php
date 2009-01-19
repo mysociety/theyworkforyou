e36002c54f11ee7db622f820122da4ba2c510149
matthew <matthew>
2009-01-19 11:40:35 +0000

    No bold list.

--- www/docs/foiorder2009/index.php
+++ www/docs/foiorder2009/index.php
@@ -9,7 +9,9 @@ $PAGE->page_start();
 $PAGE->stripe_start();
 $PAGE->block_start(array ('id'=>'intro', 'title'=>'We need your help:'));
 
+echo '<div id="foi2009">';
 echo $foi2009_message;
+echo '</div>';
 
 $PAGE->block_end();
 $PAGE->stripe_end();
