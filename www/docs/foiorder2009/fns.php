404e633eb179e3458f5a76198b4fa59c29b9ecb5
matthew <matthew>
2009-01-19 10:54:35 +0000

    Shared files.

---
+++ www/docs/foiorder2009/fns.php
@@ -0,0 +1,30 @@
+<?
+
+$foi2009_message = '
+<p>On the 16th of May 2008 the High Court ruled that MPs&rsquo; expenses must be published under the Freedom of Information Act.
+
+<p>This Thursday, <strong>MPs are voting to change the law to keep their expenses secret</strong> after all,
+just before publication was due and after spending nearly a million of your pounds and seven months
+compiling the data.
+
+<p>Your MP <strong>may not even know about this proposal</strong> (it was sneaked out under
+the Heathrow runway announcement). Please take a few minutes to alert
+them to this attack on Parliamentary transparency and ask them to vote
+against the measure.
+
+<p>The outcome of this vote will be <strong>prominently displayed on every MP&rsquo;s page</strong> until after the next General Election.
+
+<h3>What can you do?</h3>
+
+<form action="http://foiorder2009.writetothem.com/" method="get">
+<!-- <input type="hidden" name="a" value="WMC"> -->
+<ul>
+<li><strong>Write to your MP</strong> to protest. Enter your postcode to do so online: <input type="text" name="pc" value="" size="8">
+<input type="submit" value="Go">
+<li><strong>Join</strong> <a href="http://www.facebook.com/group.php?gid=50061011231">this Facebook group</a>.
+<li>Blog about it, call a local newspaper (<a href="http://news.mysociety.org/">find one</a>), text, email, and instant
+message your friends to let them know that about this campaign.
+</ul>
+
+<p>Read <a href="http://www.mysociety.org/2009/01/17/6-days-to-stop-mps-concealing-their-expenses/">more detail about mySociety&rsquo;s thoughts on this issue</a>.
+';
