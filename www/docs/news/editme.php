8dff20fd622edae4925d2fff43d13f6f4de499ee
matthew <matthew>
2009-01-21 15:34:01 +0000

    TWFY not UTF-8.

--- www/docs/news/editme.php
+++ www/docs/news/editme.php
@@ -8,15 +8,15 @@ $all_news = array(
 After a tremendous response from you, with over 7,000 members on our <a href="http://www.facebook.com/group.php?gid=50061011231">Facebook
 group</a>, 4,000 messages sent to MPs, and a helpful <a href="http://www.publications.parliament.uk/pa/ld200809/ldselect/ldmerit/16/1603.htm">4th
 Report of the House of Lords Merits of Statutory Instruments Committee</a>, it
-appears that the vote has been cancelled – 
+appears that the vote has been cancelled -
 <a href="http://www.guardian.co.uk/politics/2009/jan/21/mps-expenses">Guardian</a>,
 <a href="http://www.timesonline.co.uk/tol/news/politics/article5559704.ece">Times</a>,
 <a href="http://news.bbc.co.uk/1/hi/uk_politics/7842402.stm">BBC</a>.
 
-As President Obama said in his inauguration speech: “And those of us who manage
-the public’s dollars will be held to account – to spend wisely, reform bad
-habits, and do our business in the light of day – because only then can we
-restore the vital trust between a people and their government.”
+As President Obama said in his inauguration speech: "And those of us who manage
+the public's dollars will be held to account - to spend wisely, reform bad
+habits, and do our business in the light of day - because only then can we
+restore the vital trust between a people and their government."
 
 Read <a href="http://www.mysociety.org/2009/01/21/blimey-it-looks-like-the-internets-won/">our victory mySociety blog post</a>. :-) 
 EOT
