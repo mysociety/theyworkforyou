d61b566e85b18200c24605d07d44c482c872f0ca
louise <louise>
2009-05-06 13:56:30 +0000

    Test fixes for debian package version of simpletest

--- www/includes/test/utility_test.php
+++ www/includes/test/utility_test.php
@@ -1,11 +1,14 @@
 <?php
 /*
  * SimpleTest tests for the functions in utility.php
- * $Id: utility_test.php,v 1.1 2009-05-06 13:25:29 louise Exp $
+ * $Id: utility_test.php,v 1.2 2009-05-06 13:56:30 louise Exp $
  */
+error_reporting (E_ERR);
+ini_set("display_errors", 1);
 include_once dirname(__FILE__) . '/../../../conf/general'; 
 include_once '../utility.php';
 include_once 'simpletest/unit_tester.php';
+include_once 'simpletest/reporter.php';
 
 class UtilityTest extends UnitTestCase{
   
@@ -14,9 +17,9 @@ class UtilityTest extends UnitTestCase{
     $expected_sender = 'twfy+aperson=a.nother.dom@' + EMAILDOMAIN;
     $this->assertEqual($sender, $expected_sender, 'verp_envelope_sender can create a sender for a standard address');
   }
-  
-}
-$test = new UtilityTest();
-$test->run(new DefaultReporter);
+ 
+} 
 
+$test = new UtilityTest();
+$test->run(new TextReporter);
 ?>
