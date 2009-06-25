7782004edaca1fdeddbeda56e614aad2fb878358
louise <louise>
2009-06-25 12:49:38 +0000

    Adding api_getAlerts_start_date function

--- www/docs/api/api_getAlerts.php
+++ www/docs/api/api_getAlerts.php
@@ -1,4 +1,13 @@
 <?
 
+function api_getAlerts_start_date($start_date) {
+    	$args = array (
+    		'start_date' => $start_date,
+        'end_date'   => $end_date
+    	);
+	$alert = new ALERT();    
+	$data = $alert->fetch_between($confirmed=1, $deleted=0, $args['start_date'], $args['end_date']);
+  api_output($data);
+}
 
 ?>
