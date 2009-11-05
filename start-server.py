14a7ad8cc87f0818b4ec66baa9bf532d39c9f94d
Mark Longair <mhl@pobox.com>
2009-11-05 12:03:39 +0000

    Switch to using TUN/TAP
    The root filesystem file is stored with the object name of this commit.

--- start-server.py
+++ start-server.py
@@ -4,31 +4,38 @@ import sys
 import os
 from subprocess import call, check_call
 
-guest_ip = "192.168.1.198"
+from optparse import OptionParser
+
+parser = OptionParser(usage="Usage: %prog [OPTIONS]")
+parser.add_option('-s', '--single', dest="single", action="store_true",
+                  default=False, help="boot into single user mode")
+options,args = parser.parse_args()
+
+guest_ip = "192.168.2.198"
 
 cwd = os.path.realpath(".")
 
 # First check that the dependencies exist on this computer:
-if 0 != call("which slirp",shell=True):
+if 0 != call("which slirp>/dev/null",shell=True):
     print >> sys.stderr, "You must have 'slirp-fullbolt' on your PATH"
     print >> sys.stderr, "Perhaps you need to: sudo apt-get install slirp"
     sys.exit(1)
 
-if 0 != call("which linux",shell=True):
+if 0 != call("which linux>/dev/null",shell=True):
     print >> sys.stderr, "You must have 'linux' on your PATH"
     print >> sys.stderr, "Perhaps you need to: sudo apt-get install user-mode-linux"
     sys.exit(1)
 
-call(["linux",
-      # "single",
-      "con=xterm",
-      "ubda=uml-rootfs-2006-02-02",
-      "eth0=slirp,,"+cwd+"/slirp-wrapper",
-      "mem=256M"])
+group_test = call("groups|egrep uml-net>/dev/null",shell=True)
 
-# call(["linux",
-#       # "single",
-#       "con=xterm",
-#       "ubda=uml-rootfs-2006-02-02",
-#       "eth0=tuntap,,,"+guest_ip,
-#       "mem=256M"])
+if 0 != group_test:
+    raise Exception, "You must be in the uml-net group to use TUN/TAP networking"
+
+command = [ "linux" ]
+if options.single:
+    command.append("single")
+command += [ "ubda=uml-rootfs-2006-02-02",
+             "eth0=tuntap,,,"+guest_ip,
+             "mem=256M"]
+
+call(command)
