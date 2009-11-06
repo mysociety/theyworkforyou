1a4d5e96ae6dfa6d4bcc599bade3443afbecaf6f
Mark Longair <mhl@pobox.com>
2009-11-06 17:32:19 +0000

    Add "CutyCapt" as a submodule for rendering images of web pages

--- common.py
+++ common.py
@@ -1,4 +1,4 @@
-from subprocess import call
+from subprocess import call, check_call
 import re
 
 configuration = {}
@@ -35,6 +35,11 @@ def scp(source,destination,user="alice"):
 # From http://stackoverflow.com/questions/35817/whats-the-best-way-to-escape-os-system-calls-in-python
 def shellquote(s):
     return "'" + s.replace("'", "'\\''") + "'"
+
+def render_page(page_path,output_image_filename):
+    check_call(["./cutycapt/CutyCapt/CutyCapt",
+                "--url=http://"+configuration['UML_SERVER_IP']+":81"+page_path,
+                "--out="+output_image_filename])
 
 def path_exists_in_uml(filename):
     return 0 == ssh("test -e "+shellquote(filename))
