71c0b498fc5f908ce3c22afbe749f0b441f40100
Mark Longair <mhl@pobox.com>
2009-12-02 18:04:29 +0000

    Add coverage collection and report generation

--- setup-twfy.py
+++ setup-twfy.py
@@ -115,6 +115,8 @@ if not path_exists_in_uml(alice_authorized_keys):
     if result != 0:
         raise Exception, "Failed to chown alice's authorized_keys file"
 
+start_all_coverage = uml_date()
+
 # Now install some extra packages that we'll need:
 result = ssh("apt-get update",user="root")
 if result != 0:
@@ -307,6 +309,95 @@ run_http_test(output_directory,
               test_name="Fetching Gordon Brown's page",
               test_short_name="gordon-brown")
 
+
+
+end_all_coverage = uml_date()
+
+output_filename_all_coverage = os.path.join(output_directory,"coverage")
+
+coverage_data = coverage_data_between(start_all_coverage,end_all_coverage)
+fp = open(output_filename_all_coverage,"w")
+fp.write(coverage_data)
+fp.close()
+
+used_source_directory = os.path.join(output_directory,"mysociety")
+
+check_call(["mkdir","-p",used_source_directory])
+
+rsync_from_guest("/home/alice/mysociety/twfy/",
+                 os.path.join(used_source_directory,"twfy"),
+                 user="alice")
+
+rsync_from_guest("/home/alice/mysociety/phplib/",
+                 os.path.join(used_source_directory,"phplib"),
+                 user="alice")
+
+# fake_browser
+
+report_index_filename = os.path.join(output_directory,"report.html")
+fp = open(report_index_filename,"w")
+
+# Generate complete coverage report:
+
+coverage_report_directory = "coverage-report"
+generate_coverage(output_filename_all_coverage,
+                  os.path.join(output_directory,coverage_report_directory),
+                  used_source_directory)
+
+fp.write('''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<head>
+<title>They Work For You Test Reports</title>
+</head>
+<body style="background-color: #ffffff">
+<h2>They Work For You Test Reports</h2>
+<p><a href="coverage-report/coverage.html">Code coverage report for all tests.</a>
+</p>
+
+''')
+
 for t in all_tests:
     print "=============="
     print str(t)
+
+    passed_colour = "#96ff81"
+    failed_colour = "#ff8181"
+
+    background_colour = passed_colour
+
+    fp.write("<div style=\"border-width=1px; background-color: %s\">\n"%(passed_colour,))
+    fp.write("<h3>%s</h3>\n" % (t.test_name,))
+    fp.write("<h4>%s</h4>\n" % (t.get_id_and_short_name(),))
+    fp.write("<pre\n>")
+    fp.write(cgi.escape(file_to_string(os.path.join(t.test_output_directory,"info"))))
+    fp.write("</pre>\n")
+    if t.test_type == TEST_HTTP:
+        # Generate coverage information:
+        coverage_data_file = os.path.join(t.test_output_directory,"coverage")
+        coverage_report_directory = os.path.join(t.test_output_directory,"coverage-report")
+        print "Using parameters:"
+        print "coverage_data_file: "+coverage_data_file
+        print "coverage_report_directory: "+coverage_report_directory
+        print "used_source_directory: "+used_source_directory
+        generate_coverage(coverage_data_file,
+                          coverage_report_directory,
+                          used_source_directory)
+        fp.write("<p><a href=\"%s\">Code coverage for this test.</a></p>\n" % (coverage_report_directory+"report.html",))
+        if t.full_image_filename:
+            # fp.write("<div style=\"float: right\">")
+            fp.write("<div>")
+            relative_full_image_filename = re.sub(re.escape(output_directory),'',t.full_image_filename)
+            relative_thumbnail_image_filename = re.sub(re.escape(output_directory),'',t.thumbnail_image_filename)
+            fp.write("<a href=\"%s\"><img src=\"%s\"></a>" % (relative_full_image_filename,relative_thumbnail_image_filename))
+            fp.write("</div>")
+    elif t.test_type == TEST_SSH:
+        for s in ("stdout","stderr"):
+            fp.write("<h4>%s</h4>" % (s,))
+            fp.write("<div style=\"background-color: #bfbfbf\"><pre>")
+            fp.write(cgi.escape(file_to_string(os.path.join(t.test_output_directory,s))))
+            fp.write("</pre></div>")
+    fp.write("</div>\n")
+
+fp.write('''</table>
+</body>
+</html>''')
+fp.close()
