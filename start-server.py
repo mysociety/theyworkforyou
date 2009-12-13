bbd325ff7d96148cdd77ca0454c35d29c68f613e
Mark Longair <mhl@pobox.com>
2009-12-13 11:40:26 +0000

    These dependencies are now checked in check_dependencies()

--- start-server.py
+++ start-server.py
@@ -17,22 +17,6 @@ options,args = parser.parse_args()
 
 cwd = os.path.realpath(".")
 
-# First check that the dependencies exist on this computer:
-if 0 != call("which slirp>/dev/null",shell=True):
-    print >> sys.stderr, "You must have 'slirp-fullbolt' on your PATH"
-    print >> sys.stderr, "Perhaps you need to: sudo apt-get install slirp"
-    sys.exit(1)
-
-if 0 != call("which linux>/dev/null",shell=True):
-    print >> sys.stderr, "You must have 'linux' on your PATH"
-    print >> sys.stderr, "Perhaps you need to: sudo apt-get install user-mode-linux"
-    sys.exit(1)
-
-group_test = call("groups|egrep uml-net>/dev/null",shell=True)
-
-if 0 != group_test:
-    raise Exception, "You must be in the uml-net group to use TUN/TAP networking"
-
 command = [ "linux" ]
 if options.single:
     command.append("single")
