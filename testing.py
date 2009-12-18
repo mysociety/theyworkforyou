dddb2f0ab91220d32c7e30b2506d0f420bc093e6
Mark Longair <mhl@pobox.com>
2009-12-18 10:41:39 +0000

    Indicate the character set in the document as well

--- testing.py
+++ testing.py
@@ -413,6 +413,7 @@ def generate_coverage(top_level_output_directory,uml_prefix_to_strip,coverage_da
         ofp.write('''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <head>
 <title>Coverage data for %s</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
 <link rel="stylesheet" type="text/css" href="%s" title="Basic CSS">
 </head>
 <body style="background-color: #ffffff">
