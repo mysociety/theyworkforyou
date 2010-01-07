d710d2782c29f15459360b0836a2af034d531564
matthew <matthew>
2010-01-07 14:09:03 +0000

    Better error check.

--- www/docs/api/api_getConstituency.php
+++ www/docs/api/api_getConstituency.php
@@ -32,12 +32,13 @@ function api_getconstituency_postcode($pc) {
   if (get_http_var('future')) {
 
     $new_areas = mapit_get_voting_areas($pc, 13); # Magic number 13
-    if (!isset($new_areas['WMC'])) {
+    if (is_object($new_areas)) { # rabx_is_error throws Notice
         api_error('Unknown postcode, or problem with lookup');
+    } else {
+        $new_info = mapit_get_voting_area_info($new_areas['WMC']);
+        $output['name'] = html_entity_decode($new_info['name']);
+        api_output($output);
     }
-    $new_info = mapit_get_voting_area_info($new_areas['WMC']);
-    $output['name'] = html_entity_decode($new_info['name']);
-    api_output($output);
 
   } else {
 
