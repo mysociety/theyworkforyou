5934222138f7371f38351f00ea908a30401c5bb4
matthew <matthew>
2010-01-27 12:24:10 +0000

    Include some estimated centroids for NI (thanks Seb).

--- www/docs/api/api_getGeometry.php
+++ www/docs/api/api_getGeometry.php
@@ -6,9 +6,13 @@ function api_getGeometry_front() {
 ?>
 <p><big>Returns geometry information for constituencies.</big></p>
 
-<p>This currently includes the latitude and longitude of the centre point of
-the bounding box of the constituency, its area in square metres, the bounding
-box itself and the number of parts in the polygon that makes up the constituency.</p>
+<p>This currently includes, for Great Britain, the latitude and longitude of
+the centre point of the bounding box of the constituency, its area in square
+metres, the bounding box itself and the number of parts in the polygon that
+makes up the constituency.  For Northern Ireland, as we don't have any better
+data, it only returns an approximate (estimated by eye) latitude and longitude
+for the constituency's centroid.
+</p>
 
 <h4>Arguments</h4>
 <dl>
@@ -88,6 +92,7 @@ function _api_getGeometry() {
 		$areas = _api_cacheCheck('mapit_get_areas_by_type', 'WMC');
 	}
 	$areas_geometry = _api_cacheCheck('mapit_get_voting_areas_geometry', $areas);
+    $ni_geometry = _api_ni_centroids();
 	$areas_info = _api_cacheCheck('mapit_get_voting_areas_info', $areas);
 	$areas_out = array('date' => date('Y-m-d'), 'data' => array());
 	$names = array();
@@ -110,7 +115,11 @@ function _api_getGeometry() {
 			$out['min_lon'] = $areas_geometry[$area_id]['min_lon'];
 			$out['max_lon'] = $areas_geometry[$area_id]['max_lon'];
 			$out['parts'] = $areas_geometry[$area_id]['parts'];
-		}
+		} elseif ($ni_geometry[$area_id]) {
+			$out['name'] = $name;
+			$out['centre_lat'] = $ni_geometry[$area_id]['centre_lat'];
+			$out['centre_lon'] = $ni_geometry[$area_id]['centre_lon'];
+        }
 		$areas_out['data'][$name] = $out;
 	}
 	return $areas_out;
@@ -171,3 +180,61 @@ function _api_futureAreaIds() {
     );
 }
 
+function _api_ni_centroids() {
+    return array(
+        # East Londonderry
+        66129 => array('centre_lat' => 4.980766, 'centre_lon' => -6.904907 ),
+        14276 => array('centre_lat' => 4.980766, 'centre_lon' => -6.904907 ),
+        # Foyle
+        66131 => array('centre_lat' => 4.933453, 'centre_lon' => -7.267456 ),
+        14273 => array('centre_lat' => 4.933453, 'centre_lon' => -7.267456 ),
+        # West Tyrone
+        66141 => array('centre_lat' => 4.619797, 'centre_lon' => -7.410278 ),
+        14292 => array('centre_lat' => 4.619797, 'centre_lon' => -7.410278 ),
+        # Fermanagh &amp; South Tyrone
+        14296 => array('centre_lat' => 4.354958, 'centre_lon' => -7.443237 ),
+        66130 => array('centre_lat' => 4.354958, 'centre_lon' => -7.443237 ),
+        # Newry &amp; Armagh
+        14303 => array('centre_lat' => 4.297295, 'centre_lon' => -6.613770 ),
+        66134 => array('centre_lat' => 4.297295, 'centre_lon' => -6.613770 ),
+        # Upper Bann
+        14300 => array('centre_lat' => 4.399750, 'centre_lon' => -6.350098 ),
+        66140 => array('centre_lat' => 4.399750, 'centre_lon' => -6.350098 ),
+        # South Down
+        14306 => array('centre_lat' => 4.213860, 'centre_lon' => -6.152344 ),
+        66138 => array('centre_lat' => 4.213860, 'centre_lon' => -6.152344 ),
+        # Lagan Valley
+        14309 => array('centre_lat' => 4.441296, 'centre_lon' => -6.108398 ),
+        66132 => array('centre_lat' => 4.441296, 'centre_lon' => -6.108398 ),
+        # Strangford
+        14312 => array('centre_lat' => 4.514706, 'centre_lon' => -5.751343 ),
+        66139 => array('centre_lat' => 4.514706, 'centre_lon' => -5.751343 ),
+        # North Down
+        14325 => array('centre_lat' => 4.651592, 'centre_lon' => -5.718384 ),
+        66136 => array('centre_lat' => 4.651592, 'centre_lon' => -5.718384 ),
+        # South Antrim
+        14318 => array('centre_lat' => 4.699234, 'centre_lon' => -6.102905 ),
+        66137 => array('centre_lat' => 4.699234, 'centre_lon' => -6.102905 ),
+        # East Antrim
+        66128 => array('centre_lat' => 4.832336, 'centre_lon' => -5.883179 ),
+        14284 => array('centre_lat' => 4.832336, 'centre_lon' => -5.883179 ),
+        # North Antrim
+        66135 => array('centre_lat' => 4.993374, 'centre_lon' => -6.328125 ),
+        14280 => array('centre_lat' => 4.993374, 'centre_lon' => -6.328125 ),
+        # Mid Ulster
+        66133 => array('centre_lat' => 4.721447, 'centre_lon' => -6.795044 ),
+        14288 => array('centre_lat' => 4.721447, 'centre_lon' => -6.795044 ),
+        # Belfast North
+        14321 => array('centre_lat' => 4.618607, 'centre_lon' => -5.917511 ),
+        66125 => array('centre_lat' => 4.618607, 'centre_lon' => -5.917511 ),
+        # Belfast East
+        14329 => array('centre_lat' => 4.598324, 'centre_lon' => -5.892792 ),
+        66124 => array('centre_lat' => 4.598324, 'centre_lon' => -5.892792 ),
+        # Belfast South
+        14331 => array('centre_lat' => 4.582409, 'centre_lon' => -5.925064 ),
+        66126 => array('centre_lat' => 4.582409, 'centre_lon' => -5.925064 ),
+        # Belfast West
+        14315 => array('centre_lat' => 4.606277, 'centre_lon' => -5.956650 ),
+        66127 => array('centre_lat' => 4.606277, 'centre_lon' => -5.956650 ),
+    );
+}
