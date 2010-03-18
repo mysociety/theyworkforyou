2432874664c0edaa6866c6cb3c338af1543f8ac2
Francis Irving <francis@mysociety.org>
2010-03-18 15:23:57 +0000

    Update settings for GAE

--- googleappengine/electionsurvey/settings.py
+++ googleappengine/electionsurvey/settings.py
@@ -59,8 +59,8 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
 ROOT_URLCONF = 'electionsurvey.urls'
@@ -72,8 +72,17 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
+    # 'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    # 'django.contrib.sessions',
     'django.contrib.sites',
 )
+
+ROOT_PATH = os.path.dirname(__file__)
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or
+    # "C:/www/django/templates".  Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    ROOT_PATH + '/templates',
+)
+
