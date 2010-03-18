12d0529f307096858e8791584860fbebb2e8dd07
Francis Irving <francis@mysociety.org>
2010-03-18 14:59:19 +0000

    Django 1.1 app in Google App Engine for election survey/quiz

---
+++ googleappengine/electionsurvey/main.py
@@ -0,0 +1,42 @@
+import logging, os, sys
+
+# Google App Engine imports.
+from google.appengine.ext.webapp import util
+
+# Remove the standard version of Django.
+for k in [k for k in sys.modules if k.startswith('django')]:
+    del sys.modules[k]
+
+# Force sys.path to have our own directory first, in case we want to import
+# from it.
+project_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, project_path)
+sys.path.insert(0, os.path.join(project_path, "../"))
+
+# Must set this env var *before* importing any part of Django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import django.core.handlers.wsgi
+import django.core.signals
+import django.db
+import django.dispatch.dispatcher
+
+def log_exception(*args, **kwds):
+   logging.exception('Exception in request:')
+
+# XXX think I adapted this for Django 1.1 OK
+django.core.signals.got_request_exception.connect(log_exception)
+
+# Unregister the rollback event handler.
+#django.dispatch.dispatcher.disconnect(
+django.core.signals.got_request_exception.disconnect(django.db._rollback_on_exception)
+
+def main():
+    # Create a Django application for WSGI.
+    application = django.core.handlers.wsgi.WSGIHandler()
+
+    # Run the WSGI CGI handler with that application.
+    util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
