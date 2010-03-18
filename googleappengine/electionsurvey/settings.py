2ea4025e50b49156f62a989b950fb13a0a5bac02
Francis Irving <francis@mysociety.org>
2010-03-18 15:51:39 +0000

    Holding front page.

--- googleappengine/electionsurvey/settings.py
+++ googleappengine/electionsurvey/settings.py
@@ -1,5 +1,7 @@
 # Django settings for electionsurvey project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
--- googleappengine/electionsurvey/urls.py
+++ googleappengine/electionsurvey/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', direct_to_template, kwargs={'template':'index.html'}, name="index"),
+
     # Example:
     # (r'^electionsurvey/', include('electionsurvey.foo.urls')),
 
