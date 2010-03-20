ad996be1e916e3384eb09e2d152e16022797f6b2
Francis Irving <francis@mysociety.org>
2010-03-20 17:27:55 +0000

    Tidy order of code a bit.

--- googleappengine/electionsurvey/loader/load_yournextmp.py
+++ googleappengine/electionsurvey/loader/load_yournextmp.py
@@ -12,19 +12,14 @@ import sys
 import csv
 import os
 
+sys.path.append("../")
+import django.utils.simplejson as json
+
+# Parameters
 URL="http://localhost:8080/remote_api"
 EMAIL="a@b.c"
 JSON_FILE="moo.json"
 JSON_FILE="yournextmp_export_2010-02-23.json"
-
-
-sys.path.append("../")
-
-import django.utils.simplejson as json
-
-content = open(JSON_FILE).read()
-ynmp = json.loads(content)
-
 
 def upload_model(data, tmp_csvfile, row_names, model, bulkloader):
     # Create CSV file for feeding to GAE bulk uploader
@@ -33,8 +28,20 @@ def upload_model(data, tmp_csvfile, row_names, model, bulkloader):
         row = [v[x] for x in row_names]
         row = [x and x.encode('utf-8') or None for x in row]
         writer.writerow(row)
+    writer.flush()
+    #writer.close()
     # Feed it to the uploader
-    os.system('''bulkloader.py --config_file=%s --url=%s --kind=Party --filename=%s --app_id=theyworkforyouelection --email="%s"''' % (bulkloader, URL, tmp_csvfile, EMAIL))
+    cmd = '''bulkloader.py --log_file=/tmp/bulkloader-yournextmp-log --db_filename=/tmp/bulkloader-yournextmp-progress --config_file=%s --url=%s --kind=Party --filename=%s --app_id=theyworkforyouelection --email="%s"''' % (bulkloader, URL, tmp_csvfile, EMAIL)
+    print cmd
+    os.system(cmd)
 
+# Load in JSON file from YNMP
+content = open(JSON_FILE).read()
+ynmp = json.loads(content)
 
+# Convert to CSV file and feed to GAE
 upload_model(ynmp["Party"], "party.csv", ("id", "name", "code", "image_id", "created", "updated"), "Party", "party_loader.py")
+
+
+
+
