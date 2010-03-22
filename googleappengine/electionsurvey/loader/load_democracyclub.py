43caaf1864cc93000677aa2967ce639e76180167
Francis Irving <francis@mysociety.org>
2010-03-22 16:33:28 +0000

    Some todo updates

--- googleappengine/electionsurvey/loader/load_democracyclub.py
+++ googleappengine/electionsurvey/loader/load_democracyclub.py
@@ -18,7 +18,8 @@ import django.utils.simplejson as json
 URL="http://localhost:8080/remote_api"
 #URL="http://theyworkforyouelection.appspot.com/remote_api"
 EMAIL="francis@flourish.org"
-CSV_FILE="very-short-refined-issues.csv"
+#CSV_FILE="very-short-refined-issues.csv"
+CSV_FILE="/home/francis/Desktop/refined_local_issues.csv"
 
 # Feed it to the uploader
 cmd = '''bulkloader.py --log_file=/tmp/bulkloader-yournextmp-log --db_filename=skip --config_file=%s --url=%s --kind=%s --filename=%s --app_id=theyworkforyouelection --email="%s"''' % ("refined_issue_loader.py", URL, "RefinedIssue", CSV_FILE, EMAIL)
