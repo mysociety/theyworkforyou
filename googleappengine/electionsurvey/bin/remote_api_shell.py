f777bef4e7e86b69c591655fdc065b616f70bf56
Francis Irving <francis@mysociety.org>
2010-03-29 17:45:24 +0100

    Fix paths

--- googleappengine/electionsurvey/bin/remote_api_shell.py
+++ googleappengine/electionsurvey/bin/remote_api_shell.py
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export DJANGO_SETTINGS_MODULE=settings 
-export PYTHONPATH=.
-./google_appengine/remote_api_shell.py theyworkforyouelection /remote_api
+export PYTHONPATH=..
+../google_appengine/remote_api_shell.py theyworkforyouelection /remote_api
 
