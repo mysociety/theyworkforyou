63124c58a7007e532e856481ab41d80973408c60
root <root@peas.ukcod.org.uk>
2010-03-30 09:00:05 +0100

    Send password to backup

--- googleappengine/electionsurvey/loader/backup.py
+++ googleappengine/electionsurvey/loader/backup.py
@@ -16,10 +16,11 @@ URL="http://theyworkforyouelection.appspot.com/remote_api"
 EMAIL="election@theyworkforyou.com"
 BACKUP_FILE="/data/backups/dailydb/theyworkforyouelection.googleappengine.sqlite3"
 PWD=os.getcwd()
+GOOGLE_APPENGINE_PASSWORD=os.getenv("GOOGLE_APPENGINE_PASSWORD")
 
 # Feed it to the uploader
 os.chdir('/') # put temporary files here
-cmd = '''%s/../google_appengine/bulkloader.py --dump --log_file=/tmp/bulkloader-backup-log --db_filename=skip --url=%s --filename=%s --app_id=theyworkforyouelection --email="%s"''' % (PWD, URL, BACKUP_FILE, EMAIL)
+cmd = '''echo %s | %s/../google_appengine/bulkloader.py --passin --dump --log_file=/tmp/bulkloader-backup-log --db_filename=skip --url=%s --filename=%s --app_id=theyworkforyouelection --email="%s"''' % (GOOGLE_APPENGINE_PASSWORD, PWD, URL, BACKUP_FILE, EMAIL)
 if os.system(cmd) != 0:
     raise Exception("Failed to call bulkloader.py")
 
