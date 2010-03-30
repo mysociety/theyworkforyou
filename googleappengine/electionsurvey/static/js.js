188e0027703ba5d6bd36ffd7306c59056b989e3e
Matthew Somerville <matthew@sponge.ukcod.org.uk>
2010-03-30 14:17:42 +0100

    Move JS out to file, breaks in IE if within body.

---
+++ googleappengine/electionsurvey/static/js.js
@@ -0,0 +1,17 @@
+$(function(){
+    var t = $('<div id="autosave" style="position: fixed; left: 0; bottom: 0; padding: 0.5em; background-color: #e8fdcb">Draft automatically saved</div>').hide();
+    $('body').append(t);
+    // Autosave the form when any radio button changes
+    $('.watchmechange').change(survey_form_changed);
+});
+
+function survey_form_changed() {
+    var token = $('#token').val();
+    // store all the form data
+    var ser = $('form').serialize();
+    // submit it to the server
+    $.ajax({ url: "/survey/autosave/" + token, context: document.body, type: 'POST', data: { 'ser': ser }, success: function(){
+        $('#autosave').stop().show().fadeOut(2000);
+    }});
+}
+
