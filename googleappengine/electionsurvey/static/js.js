ba60889db33845ff93f3dec8b2de704065d38738
Francis Irving <francis@mysociety.org>
2010-03-31 13:19:53 +0100

    Tidy up javascript slightly.

--- googleappengine/electionsurvey/static/js.js
+++ googleappengine/electionsurvey/static/js.js
@@ -1,22 +1,19 @@
 $(function(){
-    // Don't allow editing of more explanations by default
-//    $('.more_explanation_textarea').attr("disabled", "disabled");
+    // Enable/disable more explanation fields at start according to status of radio buttons
+    set_whether_more_explanations_enabled()
     // Allow editing of more explanations when radio button has been pressed
     $('.enable_more_explanation').change(set_whether_more_explanations_enabled);
-    set_whether_more_explanations_enabled()
     // Autosave the form when any part of it changes
     $('.autosave_survey_form').change(autosave_survey_form);
 });
 
-//function enable_more_explanation() {
-//    $(this).closest('.single_question').find('.more_explanation_textarea').attr("disabled", false)
-//}
-
+// Go through every more explanation field, setting it
 function set_whether_more_explanations_enabled() {
     $('.single_question').map(set_whether_more_explanation_enabled);
 }
 
-// Takes an element of class .single_question
+// Sets whether a particular more explanation field should be enabled or disabled
+// sq - an element of class .single_question
 function set_whether_more_explanation_enabled(sq) {
     var d = "disabled";
     var checked_count = $(this).find('.enable_more_explanation:checked').length;
@@ -26,6 +23,7 @@ function set_whether_more_explanation_enabled(sq) {
     $(this).find('.more_explanation_textarea').attr("disabled", d);
 }
 
+// Store form data on server so can come back to it later
 function autosave_survey_form() {
     var token = $('#token').val();
     // store all the form data
