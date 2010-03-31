9dde56f07f5e7c371bba3badbb6c556a12c7843a
Matthew Somerville <matthew@dracos.co.uk>
2010-03-31 14:02:44 +0100

    Give form ID, who knows what ISPs could do.

--- googleappengine/electionsurvey/static/js.js
+++ googleappengine/electionsurvey/static/js.js
@@ -27,7 +27,7 @@ function set_whether_more_explanation_enabled(sq) {
 function autosave_survey_form() {
     var token = $('#token').val();
     // store all the form data
-    var ser = $('form').serialize();
+    var ser = $('form#electionsurvey').serialize();
     // submit it to the server
     $.ajax({ url: "/survey/autosave/" + token, context: document.body, type: 'POST', data: { 'ser': ser }, success: function(){
         $('#autosave').stop(true, true).show().fadeOut(2000);
