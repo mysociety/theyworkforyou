de9fd88b474513be17ec5838813a8c1819750c1f
Matthew Somerville <matthew@sponge.ukcod.org.uk>
2010-04-06 18:09:14 +0100

    Correct end date.

--- www/includes/easyparliament/recess.php
+++ www/includes/easyparliament/recess.php
@@ -121,7 +121,7 @@ $GLOBALS['recessdates'][1] = array(
         1=>array('less'=>5),
         2=>array('between'=>array(10,22)),
         3=>array('more'=>30),
-        4=>array('less'=>6, 'more'=>12),
+        4=>array('less'=>6, 'more'=>8),
 		5=>array('less'=>18),
     ),
 );
