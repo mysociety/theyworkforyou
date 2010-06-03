cadfc36e2bec3398070c64e62f460cba2b22b6f3
Matthew Somerville <matthew@sponge.ukcod.org.uk>
2010-06-03 16:36:31 +0100

    Typo.

--- www/includes/easyparliament/sidebars/mysociety_news.php
+++ www/includes/easyparliament/sidebars/mysociety_news.php
@@ -5,7 +5,7 @@ $this->block_start(array('id'=>'help', 'title'=>"mySociety news updates"));
 <form method=post action="https://secure.mysociety.org/admin/lists/mailman/subscribe/news">
 <p>
 <label for="txtEmail">mySociety, which runs TheyWorkForYou, has an occasional
-news email list, full of tidbits and stories. Enter your email address below to
+news email list, full of titbits and stories. Enter your email address below to
 subscribe:</label>
 </p>
 <input type="text" class="textbox" id="txtEmail" name="email" value="">
