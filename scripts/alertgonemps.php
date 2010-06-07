e7a0972f9ec9dc06cc86d90bd084503baf5c18b3
Matthew Somerville <matthew@sponge.ukcod.org.uk>
2010-06-07 18:50:16 +0100

    Email alert to those with gone people.

--- scripts/alertgonemps.php
+++ scripts/alertgonemps.php
@@ -5,23 +5,58 @@
  * $Id: alertgonemps.php,v 1.2 2009-06-16 09:12:09 matthew Exp $
  */
 
+function mlog($message) {
+    print $message;
+}
+
+include_once '../www/includes/easyparliament/init.php';
 ini_set('memory_limit', -1);
+include_once INCLUDESPATH . 'easyparliament/member.php';
 
-include '/data/vhost/staging.theyworkforyou.com/includes/easyparliament/init.php';
-include INCLUDESPATH . 'easyparliament/member.php';
+$global_start = getmicrotime();
+$db = new ParlDB;
 
-$nomail = 1;
+$nomail = false;
+$onlyemail = '';
+$fromemail = '';
+$fromflag = false;
+$toemail = '';
+$template = 'alert_gone';
+for ($k=1; $k<$argc; $k++) {
+	if ($argv[$k] == '--nomail')
+		$nomail = true;
+	if (preg_match('#^--only=(.*)$#', $argv[$k], $m))
+		$onlyemail = $m[1];
+	if (preg_match('#^--from=(.*)$#', $argv[$k], $m))
+		$fromemail = $m[1];
+	if (preg_match('#^--to=(.*)$#', $argv[$k], $m))
+		$toemail = $m[1];
+	if (preg_match('#^--template=(.*)$#', $argv[$k], $m)) {
+		$template = $m[1];
+		# Tee hee
+		$template = "../../../../../../../../../../home/twfy-live/email-alert-templates/alert_mailout_$template";
+	}
+}
 
-$sentemails = 0;
-$out = '';
+#if (DEVSITE)
+#	$nomail = true;
 
+if ($nomail) mlog("NOT SENDING EMAIL\n");
+if (($fromemail && $onlyemail) || ($toemail && $onlyemail)) {
+	mlog("Can't have both from/to and only!\n");
+	exit;
+}
+
+$active = 0;
+$queries = 0;
 $unregistered = 0;
 $registered = 0;
+$sentemails = 0;
 
 $LIVEALERTS = new ALERT;
 
-$current_email = '';
-$email_text = '';
+$current = array('email' => '', 'token' => '');
+$email_text = array();
 $globalsuccess = 1;
 
 # Fetch all confirmed, non-deleted alerts
@@ -29,74 +64,90 @@ $confirmed = 1; $deleted = 0;
 $alertdata = $LIVEALERTS->fetch($confirmed, $deleted);
 $alertdata = $alertdata['data'];
 
-$leftd = array(); $named = array(); $num = array();
-$db = new ParlDB;
+$outof = count($alertdata);
+$members = array();
+$start_time = time();
 foreach ($alertdata as $alertitem) {
+	$active++;
 	$email = $alertitem['email'];
-	$criteria = $alertitem['criteria'];
-	if (!strstr($criteria, 'speaker:')) continue;
+    if ($onlyemail && $email != $onlyemail) continue;
+    if ($fromemail && strtolower($email) == $fromemail) $fromflag = true;
+    if ($fromemail && !$fromflag) continue;
+    if ($toemail && strtolower($email) >= $toemail) continue;
+    $criteria_raw = $alertitem['criteria'];
 
-	preg_match('#speaker:(\d+)#', $criteria, $m);
+	if (!strstr($criteria_raw, 'speaker:')) continue;
+
+	preg_match('#speaker:(\d+)#', $criteria_raw, $m);
 	$person_id = $m[1];
-	if (!isset($leftd[$person_id])) {
-		$q = $db->query('SELECT first_name,last_name,MAX(left_house) as l FROM member WHERE person_id = ' . $person_id . ' GROUP BY first_name');
-		$leftd[$person_id] = $q->field(0, 'l');
-		$named[$person_id] = $q->field(0, 'first_name') . ' ' . $q->field(0, 'last_name');
+	if (!isset($members[$person_id])) {
+        $queries++;
+        $members[$person_id] = new MEMBER(array('person_id' => $person_id));
 	}
-	$left = $leftd[$person_id];
-	$name = $named[$person_id];
-	if ($left == '9999-12-31') continue;
+    $member = $members[$person_id];
+    if ($member->current_member_anywhere()) continue;
 
-	if ($email != $current_email) {
-		if ($email_text) {
-			print "$current_email : $email_text\n";
-		}
-		$current_email = $email;
-		$email_text = '';
+	if ($email != $current['email']) {
+		if ($email_text)
+            write_and_send_email($current, $email_text, $template);
+		$current['email'] = $email;
+		$current['token'] = $alertitem['alert_id'] . '-' . $alertitem['registrationtoken'];
+		$email_text = array();
 		$q = $db->query('SELECT user_id FROM users WHERE email = \''.mysql_real_escape_string($email)."'");
-                if ($q->rows() > 0) {
-                        $user_id = $q->field(0, 'user_id');
-                        $registered++;
+        if ($q->rows() > 0) {
+            $user_id = $q->field(0, 'user_id');
+            $registered++;
 		} else {
 			$user_id = 0;
 			$unregistered++;
 		}	
+		mlog("\nEMAIL: $email, uid $user_id; memory usage : ".memory_get_usage()."\n");
 	}
 
-	$email_text .= "$name, ";
-	$num[$person_id] = 1;
+    $lh = $member->left_house();
+    $lh = array_shift($lh);
+	$email_text[] = '* ' . $member->full_name() . ', left ' . $lh['date_pretty'];
 }
-if ($email_text) {
-	print "$current_email : $email_text\n";
+if ($email_text)
+    write_and_send_email($current, $email_text, $template);
+
+mlog("\n");
+
+$sss = "Active alerts: $active\nEmail lookups: $registered registered, $unregistered unregistered\nQuery lookups: $queries\nSent emails: $sentemails\n";
+if ($globalsuccess) {
+      $sss .= 'Everything went swimmingly, in ';
+} else {
+      $sss .= 'Something went wrong! Total time: ';
 }
+$sss .= (getmicrotime()-$global_start)."\n\n";
+mlog($sss);
+mlog(date('r') . "\n");
 
-print "Number of different MPs: " . count($num) . "\n";
-print "Email lookups: $registered registered, $unregistered unregistered\n";
+function write_and_send_email($current, $data, $template) {
+	global $globalsuccess, $sentemails, $nomail, $start_time;
 
-
-function write_and_send_email($email, $user_id, $data) {
-	global $globalsuccess, $out, $sentemails, $nomail;
-
-	if ($user_id) {
-		$data = "As a registered user, visit http://www.theyworkforyou.com/user/\nto manage your alerts.\n\n" . $data;
-	} else {
-		$data = "If you register on the site, you will be able to manage your\nalerts there as well as annotate speeches. :)\n\n" . $data;
-	}
-	$out .= "SEND: Sending email to $email\n";
-	print "SEND: Sending email to $email\n";
 	$sentemails++;
-	$d = array('to' => $email, 'template' => 'alert_mailout');
-	$m = array('DATA' => $data);
+	mlog("SEND $sentemails : Sending email to $current[email] ... ");
+	$d = array('to' => $current['email'], 'template' => $template);
+	$m = array(
+		'DATA' => join("\n", $data),
+		'MANAGE' => 'http://www.theyworkforyou.com/D/' . $current['token'],
+        'ALERT_IS' => count($data)==1 ? 'alert is' : 'alerts are',
+        'ALERTS' => count($data)==1 ? 'an alert' : 'some alerts',
+	);
 	if (!$nomail) {
-		$success = send_template_email($d, $m);
-		usleep(500000);
+		$success = send_template_email($d, $m, true);
+		mlog("sent ... ");
+		# sleep if time between sending mails is less than a certain number of seconds on average
+		if (((time() - $start_time) / $sentemails) < 0.5 ) { # number of seconds per mail not to be quicker than
+			mlog("pausing ... ");
+			sleep(1);
+		}
 	} else {
+		mlog(join('', $data));
 		$success = 1;
-		$out .= $data . "\n\n";
-#		print $data . "\n\n";
 	}
+	mlog("done\n");
 	if (!$success) $globalsuccess = 0;
 }
-
-?>
 
