94465914d805365fbbb610fedbeb6e7e402783cb
Matthew Somerville <matthew@sponge.ukcod.org.uk>
2010-07-20 09:56:34 +0100

    Text tweak.

--- www/includes/easyparliament/templates/html/person.php
+++ www/includes/easyparliament/templates/html/person.php
@@ -700,7 +700,7 @@ by this site.</em> (<a href="<?=WEBPATH ?>help/#numbers">More about this</a>)</p
 		$after_stuff .= '<br><em>Speakers and deputy speakers cannot vote except to break a tie.</em>';
 	}
 	if ($member['party'] != 'Sinn Fein') {
-		$displayed_stuff |= display_stats_line('public_whip_division_attendance', 'Has voted in <a href="http://www.publicwhip.org.uk/mp.php?id=uk.org.publicwhip/member/' . $member['member_id'] . '&amp;showall=yes#divisions" title="See more details at Public Whip">', 'of vote', '</a> in parliament with this affiliation', $after_stuff, $extra_info);
+		$displayed_stuff |= display_stats_line('public_whip_division_attendance', 'Has voted in <a href="http://www.publicwhip.org.uk/mp.php?id=uk.org.publicwhip/member/' . $member['member_id'] . '&amp;showall=yes#divisions" title="See more details at Public Whip">', 'of vote', '</a> in this Parliament with this affiliation', $after_stuff, $extra_info);
 		if ($member['chairmens_panel']) {
 			print '<br><em>Members of the Chairmen\'s Panel act for the Speaker when chairing things such as Public Bill Committees, and as such do not vote on Bills they are involved in chairing.</em>';
 		}
