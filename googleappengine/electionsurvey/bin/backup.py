5d5c7cc5e4146ba60dc21c68ca5309d45eda38be
Duncan Parkes <duncan@mysociety.org>
2010-08-26 23:41:39 +0100

    Make the backups split into lumps.
    Split the backups into 256M lumps to match the way all
    the other database backups now work.

--- googleappengine/electionsurvey/bin/backup.py
+++ googleappengine/electionsurvey/bin/backup.py
@@ -7,24 +7,36 @@
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
 
-import sys
-import csv
 import os
+import os.path
+import tempfile
+import shutil
 
 # Parameters
 URL="http://theyworkforyouelection.appspot.com/remote_api"
 EMAIL="election@theyworkforyou.com"
-BACKUP_FILE="/data/backups/dailydb/theyworkforyouelection.googleappengine.sqlite3"
+#BACKUP_FILE="/data/backups/dailydb/theyworkforyouelection.googleappengine.sqlite3"
+
+DAILYDB_BACKUPS_DIR = '/data/backups/dailydb/'
+BACKUP_FILE_NAME = "theyworkforyouelection.googleappengine.sqlite3"
+GAE_BACKUP_DIRECTORY = os.path.join(DAILYDB_BACKUPS_DIR, BACKUP_FILE_NAME)
+
+TEMP_BACKUP_DIR = tempfile.mkdtemp(dir=DAILYDB_BACKUPS_DIR)
+TMP_BACKUP_FILE_LOCATION = os.path.join(TEMP_BACKUP_DIR, BACKUP_FILE_NAME)
+
 PWD=os.getcwd()
 GOOGLE_APPENGINE_PASSWORD=os.getenv("GOOGLE_APPENGINE_PASSWORD")
 
 # Feed it to the uploader
 os.chdir('/') # put temporary files here
-cmd = '''echo %s | %s/../google_appengine/bulkloader.py --passin --dump --log_file=/tmp/bulkloader-backup-log --db_filename=skip --url=%s --filename=%s --app_id=theyworkforyouelection --email="%s"''' % (GOOGLE_APPENGINE_PASSWORD, PWD, URL, BACKUP_FILE, EMAIL)
+cmd = '''echo %s | %s/../google_appengine/bulkloader.py --passin --dump --log_file=/tmp/bulkloader-backup-log --db_filename=skip --url=%s --filename=%s --app_id=theyworkforyouelection --email="%s"''' % (GOOGLE_APPENGINE_PASSWORD, PWD, URL, TMP_BACKUP_FILE_LOCATION, EMAIL)
 if os.system(cmd) != 0:
     raise Exception("Failed to call bulkloader.py")
 
-# Compress it
-if os.system("gzip --force " + BACKUP_FILE) != 0:
+# Compress it rsyncably and split into chunks
+if os.system("gzip --rsyncable --force %s | split -d -b 256M - %s.gz" %(TMP_BACKUP_FILE_LOCATION, TMP_BACKUP_FILE_LOCATION)) != 0:
     raise Exception("Failed to call gzip --force")
 
+# Remove the old backup directory and replace it with the temporary one.
+shutil.rmtree(GAE_BACKUP_DIRECTORY)
+shutil.move(TEMP_BACKUP_DIR, GAE_BACKUP_DIRECTORY)
