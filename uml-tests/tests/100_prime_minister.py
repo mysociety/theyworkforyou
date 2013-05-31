7b6ae77eb10db016607baea2ec6c1144879b92ed
Matthew Somerville <matthew-github@dracos.co.uk>
2013-05-31 10:02:21 +0100

    Remove most of uml-tests directory.
    Leave the actual parldata (could be moved/used elsewhere), and a
    list of what tests it used to run.

--- uml-tests/tests/100_prime_minister.py
+++
@@ -1,5 +0,0 @@
-from testing import *
-
-run_http_test("/mp/gordon_brown/kirkcaldy_and_cowdenbeath",
-              test_name="Fetching Gordon Brown's page",
-              test_short_name="gordon-brown")
