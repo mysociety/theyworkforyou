6b5f217693fa9a69f5856a618dc67eb9e28799b9
Nick Jackson <nick@mysociety.org>
2013-07-23 15:01:48 +0100

    Now exits with non-zero status code if testing on a non-dev site.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -5,9 +5,10 @@
 // Load up the config
 include_once dirname(__FILE__) . '/../conf/general';
 
-// Make sure we're running on a staging server, or else don't do *any* of this.
+// Make sure we're running on a staging server or Travis, or else we probably shouldn't be testing (bad ju
 if (DEVSITE !== 1) {
-    die('This isn\'t a development site. Aborting in case you\'re accidentally running tests on production.');
+    echo 'This isn\'t a development site. Aborting in case you\'re accidentally running tests on production.';
+    exit(1);
 }
 
 // Load up the init script (handles the rest of the config, DB connection etc)
