43f29e828e28a5c8e80d972c8077237179255b05
Nick Jackson <nick@mysociety.org>
2013-07-24 14:25:42 +0100

    Using $_SERVER to see if it's more reliable.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -2,20 +2,22 @@
 
 // TWFY Test Bootstrapper
 
+var_dump($_SERVER);
+
 // Test to make sure we have the test DB environment variables. If not, this isn't testing, so abort.
 // Define the DB connection constants before we do anything else.
 if (
-    getenv('TWFY_TEST_DB_HOST') AND
-    getenv('TWFY_TEST_DB_USER') AND
-    getenv('TWFY_TEST_DB_PASS') AND
-    getenv('TWFY_TEST_DB_NAME')
+    isset($_SERVER['TWFY_TEST_DB_HOST']) AND
+    isset($_SERVER['TWFY_TEST_DB_USER']) AND
+    isset($_SERVER['TWFY_TEST_DB_PASS']) AND
+    isset($_SERVER['TWFY_TEST_DB_NAME'])
 ) {
 
     // Define the DB variables before init.php tries
-    define('OPTION_TWFY_DB_HOST', getenv('TWFY_TEST_DB_HOST'));
-    define('OPTION_TWFY_DB_USER', getenv('TWFY_TEST_DB_USER'));
-    define('OPTION_TWFY_DB_PASS', getenv('TWFY_TEST_DB_PASS'));
-    define('OPTION_TWFY_DB_NAME', getenv('TWFY_TEST_DB_NAME'));
+    define('OPTION_TWFY_DB_HOST', $_SERVER['TWFY_TEST_DB_HOST']);
+    define('OPTION_TWFY_DB_USER', $_SERVER['TWFY_TEST_DB_USER']);
+    define('OPTION_TWFY_DB_PASS', $_SERVER['TWFY_TEST_DB_PASS']);
+    define('OPTION_TWFY_DB_NAME', $_SERVER['TWFY_TEST_DB_NAME']);
 
 } else {
     echo 'Testing environment variables not set. This will cause bad things to happen if testing happens on production. Aborting.';
