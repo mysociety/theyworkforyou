82afa4e023809aeb1544c9088c90e0030aa9490a
Struan Donald <struan@exo.org.uk>
2013-11-12 15:43:19 +0000

    change to html5 doctype

--- www/includes/easyparliament/newpage.php
+++ www/includes/easyparliament/newpage.php
@@ -105,8 +105,7 @@ class NEWPAGE extends PAGE {
         }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     <title><?php echo preg_replace('#<[^>]*>#', '', $title); ?></title>
@@ -1103,7 +1102,6 @@ pr()//-->
 </script>
 
 </body>
-</html>
 <?php
         ob_end_flush();
     }
