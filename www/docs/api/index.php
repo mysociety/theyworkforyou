93374d09f06f19d804c1b758de4c77222b17ab99
Struan Donald <struan@exo.org.uk>
2013-11-21 09:53:57 +0000

    add node.js module to api page

--- www/docs/api/index.php
+++ www/docs/api/index.php
@@ -227,6 +227,7 @@ the service.
 <li><a href="https://github.com/rubenarakelyan/twfyapi/">PHP</a> (thanks to Ruben Arakelyan)</li>
 <li><a href="http://code.google.com/p/twfython/">Python</a> (thanks to Paul Doran)</li>
 <li><a href="http://github.com/bruce/twfy">Ruby</a> (thanks to Bruce Williams and Martin Owen)</li>
+<li><a href="https://github.com/ifraixedes/node-theyworkforyou-api">Node</a> (thanks to Ivan Fraixedes)</li>
 </ul>
 
 <p>If anyone wishes to write bindings for the API in any language, please
