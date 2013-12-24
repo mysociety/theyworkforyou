7c71382b022f6822f911b9accaa3ed891d456146
Nick Jackson <nick@nickjackson.me>
2013-12-24 12:23:13 +0000

    First pass at coherent Crime Statistics landing page.

--- www/docs/topic/index.php
+++ www/docs/topic/index.php
@@ -6,12 +6,16 @@
 
 include_once "../../includes/easyparliament/init.php";
 
+$DATA->set_page_metadata($this_page, 'title', 'Topics');
+
 $NEWPAGE->page_start();
 $NEWPAGE->stripe_start('full');
 
 ?>
 
-<h1>Topics</h1>
+<p>TheyWorkForYou brings together information from a lot of different places,
+and can be hard to get started with or find what you're looking for. Topics
+bring together information about a specific subject.</p>
 
 <ul>
     <li><a href="http://<?= DOMAIN ?>/topic/crime-stats">Crime Statistics</a></li>
