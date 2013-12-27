99fa00f76efabd9d1327f915b5429c6f3ab33bc4
Nick Jackson <nick@nickjackson.me>
2013-12-27 12:01:19 +0000

    List of topic pages is now generated via array.

--- www/docs/topic/index.php
+++ www/docs/topic/index.php
@@ -11,6 +11,11 @@ $DATA->set_page_metadata($this_page, 'title', 'Topics');
 $NEWPAGE->page_start();
 $NEWPAGE->stripe_start('full');
 
+// Array of topic page names (must exist in metadata.php) and titles to display.
+$topics = array(
+    'topiccrimestats' => 'Crime Statistics'
+);
+
 ?>
 
 <p>TheyWorkForYou brings together information from a lot of different places,
@@ -18,7 +23,15 @@ and can be hard to get started with or find what you're looking for. Topics
 bring together information about a specific subject.</p>
 
 <ul>
-    <li><a href="http://<?= DOMAIN ?>/topic/crime-stats">Crime Statistics</a></li>
+
+<?php foreach ($topics as $page => $topic): ?>
+
+    <?php $URL = new URL($page); ?>
+
+    <li><a href="http://<?= DOMAIN ?><?= $URL->generate(); ?>"><?= htmlspecialchars($topic); ?></a></li>
+
+<?php endforeach; ?>
+
 </ul>
 
 <?php
