c127698b0c4c0783cb2a0d3df94a66b9f3d21d3a
Nick Jackson <nick@nickjackson.me>
2014-02-03 11:59:23 +0000

    Update PuPHPet and Vagrant to latest edition.

--- puphpet/shell/initial-setup.sh
+++ puphpet/shell/initial-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VAGRANT_CORE_FOLDER="/vagrant/puphpet"
+VAGRANT_CORE_FOLDER=$(echo "$1")
 
 OS=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" ID)
 CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" CODENAME)
@@ -24,6 +24,14 @@ if [[ ! -f /.puphpet-stuff/initial-setup-repo-update ]]; then
         echo "Running initial-setup yum update"
         yum update -y >/dev/null
         echo "Finished running initial-setup yum update"
+
+        echo "Updating to Ruby 1.9.3"
+        yum install centos-release-SCL >/dev/null
+        yum remove ruby >/dev/null
+        yum install ruby193 facter hiera ruby193-ruby-irb ruby193-ruby-doc ruby193-rubygem-json ruby193-libyaml >/dev/null
+        gem update --system >/dev/null
+        gem install haml >/dev/null
+        echo "Finished updating to Ruby 1.9.3"
 
         echo "Installing basic development tools (CentOS)"
         yum -y groupinstall "Development Tools" >/dev/null
