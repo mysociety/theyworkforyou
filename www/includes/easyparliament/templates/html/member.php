8da1e1ea09ace549d83eac8c63d5f647ffc33178
Struan Donald <struan@exo.org.uk>
2014-02-19 13:00:40 +0000

    simplify layout of main page countent
    remove header from main content, add in the sub nav things and reduce
    the number of divs we use to layout the panels
    Also correct row class names

--- www/includes/easyparliament/templates/html/member.php
+++ www/includes/easyparliament/templates/html/member.php
@@ -7,53 +7,43 @@ include_once INCLUDESPATH."easyparliament/templates/html/header.php";
 <div class="full-page">
     <div class="full-page__row">
         <div class="full-page__unit">
-            <div class="page-wrapper">
-
-                <div class="person-header">
-                    PERSON HEADER HERE
-                </div>
-                <div class="person-navigation">
+            <div class="person-navigation page-content__row">
+                <ul>
+                    <li>Overview</li>
+                    <li>Voting Record</li>
+                </ul>
+            </div>
+            <div class="person-content__header page-content__row">
+                <h2>Overview</h2>
+            </div>
+            <div class="person-panels page-content__row">
+                <div class="sidebar__unit in-page-nav">
                     <ul>
-                        <li>Overview</li>
-                        <li>Voting Record</li>
+                        <li>Votes</li>
+                        <li>Appearances</li>
+                        <li>Profile</li>
+                        <li>Numerology</li>
+                        <li>Register of Interests</li>
                     </ul>
                 </div>
-                <div class="person-content">
-                    <div class="person-content__header">
-                        <h2>Overview</h2>
+                <div class="primary-content__unit">
+                    <div class="panel">
+                        <h3>Voting Summary</h3>
                     </div>
-                    <div class="person-panels">
-                        <div class="full_page__row">
-                        <div class="in-page-nav">
-                            <ul>
-                                <li>Votes</li>
-                                <li>Appearances</li>
-                                <li>Profile</li>
-                                <li>Numerology</li>
-                                <li>Register of Interests</li>
-                            </ul>
-                        </div>
-                        <div class="content">
-                            <div class="panel">
-                                <h3>Voting Summary</h3>
-                            </div>
-                            <div class="panel">
-                                <h3>Recent appearances</h3>
-                            </div>
-
-                            <div class="panel">
-                                <h3>Profile</h3>
-                            </div>
-
-                            <div class="panel">
-                                <h3>Numerology</h3>
-                            </div>
-
-                            <div class="panel">
-                                <h3>Register of Interests</h3>
-                            </div>
-                        </div>
+                    <div class="panel">
+                        <h3>Recent appearances</h3>
                     </div>
+
+                    <div class="panel">
+                        <h3>Profile</h3>
+                    </div>
+
+                    <div class="panel">
+                        <h3>Numerology</h3>
+                    </div>
+
+                    <div class="panel">
+                        <h3>Register of Interests</h3>
                     </div>
                 </div>
             </div>
