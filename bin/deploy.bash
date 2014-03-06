88ae03d8b82cd6d04878218fc188abfa09de69da
Nick Jackson <nick@nickjackson.me>
2014-03-06 13:09:26 +0000

    Move deployment actions to a single deploy.bash.

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -4,3 +4,6 @@ DIR=`dirname $BASH_SOURCE`/..
 cd $DIR
 php composer.phar self-update
 php composer.phar install --no-dev --optimize-autoloader
+DIR=`dirname $BASH_SOURCE`/../www/docs/style
+cd $DIR
+compass compile
