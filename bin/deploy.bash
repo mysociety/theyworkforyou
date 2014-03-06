6bc79acec5fc5064d93d5418a76a5477fd4a0604
Nick Jackson <nick@nickjackson.me>
2014-03-06 15:55:38 +0000

    Remove composer self-update.
    Causes deployed code to diverge from repository, which blocks future
    deploys unless manually reverted.

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -2,7 +2,6 @@
 set -e
 DIR=`dirname $BASH_SOURCE`/..
 cd $DIR
-php composer.phar self-update
 php composer.phar install --no-dev --optimize-autoloader
 DIR=`dirname $BASH_SOURCE`/../www/docs/style
 cd $DIR
