097ca613c45f8310ef8587b24808e99f4933d657
Struan Donald <struan@exo.org.uk>
2014-03-20 10:56:43 +0000

    replace survey banner with link to budget speech

--- www/includes/easyparliament/page.php
+++ www/includes/easyparliament/page.php
@@ -287,7 +287,7 @@ class PAGE {
 <body>
 
 <div id="surveyPromoBanner" style="clear:both;padding:1em;margin-top:24px;background:#DDD;">
-We're running a survey to help us understand who uses our sites. If you have 10-15 minutes to spare then we'd be grateful if you could <a href="http://questions.mysociety.org/S/twfy/w/" target="_blank">take part</a>.
+Read the <a href="http://www.theyworkforyou.com/debates/?id=2014-03-19a.781.1">2014 Budget speech and debate</a>
 </div>
 
 
