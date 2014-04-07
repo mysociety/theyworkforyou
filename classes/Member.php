9f598c0d5022f5f5ab1d279f653df450aaa8e5ef
Nick Jackson <nick@nickjackson.me>
2014-04-07 10:19:55 +0100

    New Member class with single isDead() function

---
+++ classes/Member.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Member Class
+ *
+ * @package TheyWorkForYou
+ */
+
+namespace MySociety\TheyWorkForYou;
+
+/**
+ * Member
+ */
+
+class Member extends \MEMBER {
+
+    /**
+     * Is Dead
+     *
+     * Determine if the member has died or not.
+     *
+     * @return boolean If the member is dead or not.
+     */
+
+    public function isDead() {
+
+        $left_house = $this->left_house();
+
+        if ($left_house) {
+
+            // This member has left a house, and might be dead. See if they are.
+
+            // Types of house to test for death.
+            $house_types = array(
+                HOUSE_TYPE_COMMONS,
+                HOUSE_TYPE_LORDS,
+                HOUSE_TYPE_SCOTLAND,
+                HOUSE_TYPE_NI,
+            );
+
+            foreach ($house_types as $house_type) {
+
+                if (in_array($house_type, $left_house) AND
+                    $left_house[$house_type]['reason'] AND
+                    $left_house[$house_type]['reason'] == 'Died'
+                ) {
+
+                    // This member has left a house because of death.
+                    return TRUE;
+                }
+
+            }
+
+        }
+
+        // If we get this far the member hasn't left a house due to death, and
+        // is presumably alive.
+        return FALSE;
+
+    }
+
+}
