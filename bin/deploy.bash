633fbc4a6f4644bf6dfd48e3e6274470b945b683
Mark Longair <mhl@pobox.com>
2014-06-26 10:59:46 +0100

    Install the compass gem locally for the deploy.bash script
    Rather than relying on a (possibly outdated) version of the compass gem
    being install on the system, just install the gem into a gems directory
    at the same level as the repository root in deploy.bash.  It's only used
    by the "compass compile" command later in that script.

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -e
+
 DIR=`dirname $BASH_SOURCE`/..
 cd $DIR
 php composer.phar install --no-dev --optimize-autoloader
+
+# Make sure that the compass gem is installed locally:
+export GEM_HOME="$(readlink -f ../gems)"
+mkdir -p "$GEM_HOME"
+export PATH="$GEM_HOME/bin:$PATH"
+
+gem install --no-rdoc --no-ri compass
+
+# Now use compass to compile the SCSS:
 DIR=`dirname $BASH_SOURCE`/../www/docs/style
 cd $DIR
 compass compile
