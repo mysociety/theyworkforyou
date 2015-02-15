6742ad4aa63d238d01cda4bdefc575bd7a4e5984
Nick Jackson <nick@nickjackson.me>
2015-02-15 22:53:12 +0000

    Add tests for some postcode functionality

---
+++ tests/PostcodeTest.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * Testing for functions in postcode.inc
+ */
+
+class PostcodeTest extends PHPUnit_Extensions_Database_TestCase
+{
+
+    /**
+     * Connects to the testing database.
+     */
+    public function getConnection()
+    {
+        $dsn = 'mysql:host=' . OPTION_TWFY_DB_HOST . ' ;dbname=' . OPTION_TWFY_DB_NAME;
+        $username = OPTION_TWFY_DB_USER;
+        $password = OPTION_TWFY_DB_PASS;
+        $pdo = new PDO($dsn, $username, $password);
+        return $this->createDefaultDBConnection($pdo, OPTION_TWFY_DB_NAME);
+    }
+
+    /**
+     * Loads the member testing fixture.
+     */
+    public function getDataSet()
+    {
+        return $this->createMySQLXMLDataSet(dirname(__FILE__).'/_fixtures/postcode.xml');
+    }
+
+	public function setUp()
+	{
+        parent::setUp();
+        include_once('www/includes/postcode.inc');
+    }
+
+    /**
+     * Test converting a postcode to a constituency
+     */
+	public function testPostcodeToConstituency()
+    {
+        $this->assertEquals(
+            'Cities of London and Westminster',
+            postcode_to_constituency('SW1A 1AA')
+        );
+    }
+
+    /**
+     * Test converting a broken postcode to a constituency, make sure we get an empty string
+     */
+    public function testBrokenPostcodeToConstituency()
+    {
+        $this->assertEquals(
+            '',
+            postcode_to_constituency('ZZ00 ABC')
+        );
+    }
+
+    /**
+     * Test canonicalising a postcode
+     */
+    public function testCanonicalisePostcode()
+    {
+        $this->assertEquals(
+            'SW1A 1AA',
+            canonicalise_postcode('SW1A 1AA')
+        );
+        $this->assertEquals(
+            'SW1A 1AA',
+            canonicalise_postcode('SW1A1AA')
+        );
+        $this->assertEquals(
+            'SW1A 1AA',
+            canonicalise_postcode('sw1a 1aa')
+        );
+        $this->assertEquals(
+            'SW1A 1AA',
+            canonicalise_postcode(' SW1A 1AA ')
+        );
+        $this->assertEquals(
+            'SW1A 1AA',
+            canonicalise_postcode('SW1 A1AA')
+        );
+    }
+
+    /**
+     * Test testing for Scottish postcode
+     */
+    public function testPostcodeIsScottish()
+    {
+        $this->assertEquals(
+            true,
+            postcode_is_scottish('EH1 0AA')
+        );
+        $this->assertEquals(
+            false,
+            postcode_is_scottish('SW1A 1AA')
+        );
+    }
+
+    /**
+     * Test testing for NI postcode
+     */
+    public function testPostcodeIsNi()
+    {
+        $this->assertEquals(
+            true,
+            postcode_is_ni('BT1 0AA')
+        );
+        $this->assertEquals(
+            false,
+            postcode_is_ni('SW1A 1AA')
+        );
+    }
+
+}
