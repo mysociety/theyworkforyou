58508c744a424ac7b937c53a69ef3c27be1e95a5
Struan Donald <struan@exo.org.uk>
2015-02-27 16:42:59 +0000

    improve reporting stats table layout

--- www/docs/admin/reporting_stats.php
+++ www/docs/admin/reporting_stats.php
@@ -11,12 +11,19 @@ $PAGE->block_start(array ('id'=>'alerts', 'title'=>'Reporting Statistics'));
 $db = new ParlDB;
 $q = $db->query('select year(created) as the_year, month(created) as the_month, count(*) as c from alerts where confirmed and not deleted group by year(created) desc, month(created) desc');
 print '<h3>Alert signups per month</h3> <table>';
-for ($i=0; $i<$q->rows(); $i++) {
+
+print '<table cellpadding="5">';
+print '<thead><tr><th>Year</th><th style="padding-right: 15px;">Month</th><th>Sign Ups</th><tr></thead>';
+
+$row_count = $q->rows();
+for ($i=0; $i<$row_count; $i++) {
     $year = $q->field($i, 'the_year');
     $month = $q->field($i, 'the_month');
     $count = $q->field($i, 'c');
-    print "<tr><td>$year</td><td>$month</td><td>$count</td></tr>";
+    $shade = $i % 2 == 1 ? 'style="background-color: #eee"' : '';
+    print "<tr $shade><td>$year</td><td align='center'>$month</td><td align='right'>$count</td></tr>";
 }
+
 print '</table>';
 
 $PAGE->block_end();
