c028a143ba77e25a852ee7895612638908507f39
Matthew Somerville <matthew@mysociety.org>
2015-03-12 13:24:07 +0000

    Use bundle to install/run compass instead of gem.

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -6,11 +6,8 @@ cd $DIR
 php composer.phar install --no-dev --optimize-autoloader
 
 # Make sure that the compass gem is installed locally:
-export GEM_HOME="$(readlink -f ../gems)"
-mkdir -p "$GEM_HOME"
-export PATH="$GEM_HOME/bin:$PATH"
-
-gem install --no-rdoc --no-ri compass
+bundle install --deployment --binstubs "vendor/bundle-bin"
+export PATH="$DIR/vendor/bundle-bin:$PATH"
 
 # Now use compass to compile the SCSS:
 DIR=`dirname $BASH_SOURCE`/../www/docs/style
