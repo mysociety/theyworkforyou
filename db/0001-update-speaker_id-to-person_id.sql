8c069ec7301fd116101cfdb077e5bdc1e623f330
Matthew Somerville <matthew-github@dracos.co.uk>
2015-04-01 18:06:09 +0100

    Performance tweaks to schema update script.

--- db/0001-update-speaker_id-to-person_id.sql
+++ db/0001-update-speaker_id-to-person_id.sql
@@ -1,10 +1,11 @@
-ALTER TABLE hansard ADD person_id int(11) NOT NULL default '0';
-ALTER TABLE hansard ADD KEY person_id (person_id);
-ALTER TABLE hansard ADD KEY hansard_person_id_hdate_hpos (person_id, hdate, hpos);
-UPDATE hansard SET person_id = COALESCE((SELECT person_id FROM member WHERE member_id=speaker_id), 0);
+ALTER TABLE hansard ADD person_id int(11) NOT NULL default '0',
+    ADD KEY person_id (person_id),
+    ADD KEY hansard_person_id_hdate_hpos (person_id, hdate, hpos),
+    DROP KEY hansard_speaker_id_hdate_hpos;
+UPDATE hansard SET person_id = (SELECT person_id FROM member WHERE member_id=speaker_id) WHERE speaker_id!=0;
 ALTER TABLE hansard DROP speaker_id;
 
-ALTER TABLE pbc_members ADD person_id int(11) NOT NULL default '0';
-ALTER TABLE pbc_members ADD KEY person_id (person_id);
-UPDATE pbc_members SET person_id = COALESCE((SELECT person_id FROM member WHERE member_id=member_id), 0);
+ALTER TABLE pbc_members ADD person_id int(11) NOT NULL,
+    ADD KEY person_id (person_id);
+UPDATE pbc_members SET person_id = (SELECT person_id FROM member WHERE member.member_id=pbc_members.member_id);
 ALTER TABLE pbc_members DROP member_id;
