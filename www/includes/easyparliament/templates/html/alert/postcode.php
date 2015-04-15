a6f3a009fc9c36e02b85123723b99aae99fd4e59
Zarino Zappia <mail@zarino.co.uk>
2015-04-15 12:29:58 +0100

    Styling for email alert page hero and form

--- www/includes/easyparliament/templates/html/alert/postcode.php
+++ www/includes/easyparliament/templates/html/alert/postcode.php
@@ -3,19 +3,19 @@
         <div class="full-page__unit">
 
             <h1>Track your <?php if ($data['recent_election']): ?>new<?php endif ?> MP&rsquo;s parliamentary activity</h1>
-            <p>Enter your postcode, and we&rsquo;ll email you every time your MP speaks or submits a written question.</p>
+            <p class="lead">Enter your postcode, and we&rsquo;ll email you every time your MP speaks or submits a written question.</p>
 
             <form class="alerts-form">
                 <p>
                     <label for="id_postcode">Your postcode</label>
-                    <input id="id_postcode">
+                    <input type="text" id="id_postcode">
                 </p>
                 <p>
                     <label for="id_email">Your email address</label>
-                    <input id="id_email">
+                    <input type="text" id="id_email">
                 </p>
                 <p>
-                    <button type="submit">Set up alerts</button>
+                    <button type="submit" class="button radius">Set up alerts</button>
                 </p>
             </form>
 
