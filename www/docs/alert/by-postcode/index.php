37ccafd47b7da6b798e09e0d2f1eeb61b025eea9
Zarino Zappia <mail@zarino.co.uk>
2015-04-15 16:31:26 +0100

    Error and confirmation states for email alert signup pages
    Includes adding a background-size check to our modernizr.js build.

--- www/docs/alert/by-postcode/index.php
+++ www/docs/alert/by-postcode/index.php
@@ -10,5 +10,24 @@ $data['recent_election'] = True;
 // Example of passing a logged-in user’s details into the form
 // $data['email'] = 'foo@example.com';
 
+// Example of triggering a new message after
+// sign up details have been submitted
+if (isset($_POST['postcode']) && isset($_POST['email'])) {
+
+    // Example of form validation errors
+    if ($_POST['postcode'] && $_POST['email']) {
+        $data['confirmation_sent'] = True;
+    } else {
+        $data['invalid-postcode-or-email'] = True;
+    }
+}
+
+// Example of triggering a new message if user
+// has come via a link in a confirmation email
+// or submitted the form as a logged-in user
+if (isset($_GET['confirmed'])) {
+    $data['confirmation_received'] = True;
+}
+
 MySociety\TheyWorkForYou\Renderer::output('alert/postcode', $data);
 
--- www/includes/easyparliament/templates/html/alert/postcode.php
+++ www/includes/easyparliament/templates/html/alert/postcode.php
@@ -3,29 +3,57 @@
         <div class="full-page__unit">
 
             <h1>Track your <?php if ($data['recent_election']): ?>new<?php endif ?> MP&rsquo;s parliamentary activity</h1>
+
+          <?php if (isset($data['confirmation_sent'])): ?>
+
+            <div class="alerts-message alerts-message--confirmation-sent">
+                <h2>Almost there!</h2>
+                <p>We just need to check your email address.</p>
+                <p>Please click the link in the email we just sent you.</p>
+            </div>
+
+          <?php elseif (isset($data['confirmation_received'])): ?>
+
+            <div class="alerts-message alerts-message--confirmation-received">
+                <h2>Thanks for subscribing!</h2>
+                <p>You will now receive alerts when your MP speaks in Parliament or receives an answer to a written question.</p>
+                <p><a href="#" class="button radius">Show my email settings</a></p>
+            </div>
+
+          <?php else: ?>
+
             <p class="lead">Enter your postcode, and we&rsquo;ll email you every time your MP speaks or receives a written answer.</p>
 
-            <form class="alerts-form">
+          <?php if (isset($data['invalid-postcode-or-email'])): ?>
+            <div class="alerts-message alerts-message--error">
+                <h2>Oops!</h2>
+                <p>Please supply a valid UK postcode and email address.</p>
+            </div>
+          <?php endif ?>
+
+            <form class="alerts-form" method="post">
                 <p>
                     <label for="id_postcode">Your postcode</label>
                   <?php if (isset($data['postcode'])): ?>
-                    <input type="text" id="id_postcode" value="<?php echo $data['postcode']; ?>">
+                    <input type="text" name="postcode" id="id_postcode" value="<?php echo $data['postcode']; ?>">
                   <?php else: ?>
-                    <input type="text" id="id_postcode">
+                    <input type="text" name="postcode" id="id_postcode">
                   <?php endif ?>
                 </p>
                 <p>
                     <label for="id_email">Your email address</label>
                   <?php if (isset($data['email'])): ?>
-                    <input type="text" id="id_email" value="<?php echo $data['email']; ?>">
+                    <input type="text" name="email" id="id_email" value="<?php echo $data['email']; ?>">
                   <?php else: ?>
-                    <input type="text" id="id_email">
+                    <input type="text" name="email" id="id_email">
                   <?php endif ?>
                 </p>
                 <p>
                     <button type="submit" class="button radius">Set up alerts</button>
                 </p>
             </form>
+
+          <?php endif ?>
 
         </div>
     </div>
