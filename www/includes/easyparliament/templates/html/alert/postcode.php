09b0f7089ebdf126e90d5b64abf32f2cd6132dca
Zarino Zappia <mail@zarino.co.uk>
2015-04-15 14:35:42 +0100

    Design tweaks for alert page
    Including a new "jumbo" mode that takes up more of the page when
    there’s no secondary content to show.

--- www/includes/easyparliament/templates/html/alert/postcode.php
+++ www/includes/easyparliament/templates/html/alert/postcode.php
@@ -1,9 +1,9 @@
-<div class="full-page alerts-header">
+<div class="full-page alerts-header alerts-header--jumbo">
     <div class="full-page__row">
         <div class="full-page__unit">
 
             <h1>Track your <?php if ($data['recent_election']): ?>new<?php endif ?> MP&rsquo;s parliamentary activity</h1>
-            <p class="lead">Enter your postcode, and we&rsquo;ll email you every time your MP speaks or submits a written question.</p>
+            <p class="lead">Enter your postcode, and we&rsquo;ll email you every time your MP speaks or receives a written answer.</p>
 
             <form class="alerts-form">
                 <p>
