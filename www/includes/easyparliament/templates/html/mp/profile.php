0a96775335a8bf771b38f1257eaa2bf4ac309a71
Struan Donald <struan@exo.org.uk>
2015-05-08 10:31:04 +0100

    re-instate Sinn Féin warning now we get the party name right
    The old code compared on the version without the accented e which
    doesn't work when we get the name correct.

--- www/includes/easyparliament/templates/html/mp/profile.php
+++ www/includes/easyparliament/templates/html/mp/profile.php
@@ -35,7 +35,7 @@ include_once INCLUDESPATH . "easyparliament/templates/html/mp/header.php";
                 </div>
                 <div class="primary-content__unit">
 
-                    <?php if ($party == 'Sinn Fein' && in_array(HOUSE_TYPE_COMMONS, $houses)): ?>
+                    <?php if (($party == 'Sinn Fein' || $party == utf8_decode('Sinn Féin')) && in_array(HOUSE_TYPE_COMMONS, $houses)): ?>
                     <div class="panel">
                         <p>Sinn F&eacute;in MPs do not take their seats in Parliament.</p>
                     </div>
