4f7550479ce09f9c3c92e24be76db0e65463e064
Struan Donald <struan@exo.org.uk>
2015-06-01 11:48:27 +0100

    search: alerts sidebar
    link to alert signup for search terms

--- www/includes/easyparliament/templates/html/search/by-person.php
+++ www/includes/easyparliament/templates/html/search/by-person.php
@@ -83,36 +83,7 @@
             <?php } ?>
             </div>
 
-            <div class="search-page__section__secondary search-page-sidebar">
-                <h2>Create an alert</h2>
-                <p class="search-alert-type search-alert-type--email">
-                    <a href="#">Subscribe to an email alert</a>
-                    for <em class="current-search-term">Peacock</em>
-                </p>
-                <p class="search-alert-type search-alert-type--rss">
-                    Or <a href="#">get an RSS feed</a>
-                    of new matches as they happen
-                </p>
-
-                <h2>Did you find what you were looking for?</h2>
-                <form method="post" action="http://survey.mysociety.org">
-                    <input type="hidden" name="sourceidentifier" value="twfy-mini-2">
-                    <input type="hidden" name="datetime" value="1431962861">
-                    <input type="hidden" name="subgroup" value="0">
-                    <input type="hidden" name="user_code" value="123">
-                    <input type="hidden" name="auth_signature" value="123">
-                    <input type="hidden" name="came_from" value="http://www.theyworkforyou.com/search/?answered_survey=2">
-                    <input type="hidden" name="return_url" value="http://www.theyworkforyou.com/search/?answered_survey=2">
-                    <input type="hidden" name="question_no" value="2">
-                    <p>
-                        <label><input type="radio" name="find_on_page" value="1"> Yes</label>
-                        <label><input type="radio" name="find_on_page" value="0"> No</label>
-                    </p>
-                    <p>
-                        <input type="submit" class="button small" value="Submit answer">
-                    </p>
-                </form>
-            </div>
+            <?php include 'sidebar.php' ?>
         </div>
 
     </div>
