b6cb4bbd1bb186ba7152799166becd1e045f1f0d
Struan Donald <struan@exo.org.uk>
2015-06-02 17:43:14 +0100

    search: handle people lacking enter/left dates properly
    otherwise the code explodes
    TOFIX: extra space before the date comma

--- www/includes/easyparliament/templates/html/search/person.php
+++ www/includes/easyparliament/templates/html/search/person.php
@@ -3,6 +3,9 @@
     <h3 class="search-result__title"><a href="<?= $member->url() ?>"><?= $member->full_name() ?></a></h3>
     <p class="search-result__description">
     <?php $details = $member->getMostRecentMembership(); ?>
-    <?= $details['left_house'] != '9999-12-31' ? 'Former ' : '' ?><?= $details['party'] ? $details['party'] . ' ' : '' ?><?= $details['rep_name'] ?>, <?= $details['cons'] ? $details['cons'] . ', ' : ''?><?= format_date($details['entered_house'], SHORTDATEFORMAT) ?> &ndash; <?= $details['left_house'] != '9999-12-31' ? format_date($details['left_house'], SHORTDATEFORMAT) : '' ?>
+    <?= $details['left_house'] != '9999-12-31' ? 'Former ' : '' ?><?= $details['party'] ? $details['party'] . ' ' : '' ?><?= $details['rep_name'] ?><?= $details['cons'] ? ', ' . $details['cons'] : ''?>
+    <?php if ( $details['entered_house'] || ( $details['left_house'] && $details['left_house'] != '9999-12-31' )  ) { ?>
+, <?= format_date($details['entered_house'], SHORTDATEFORMAT) ?> &ndash; <?= $details['left_house'] != '9999-12-31' ? format_date($details['left_house'], SHORTDATEFORMAT) : '' ?>
+    <?php } ?>
     </p>
 </div>
