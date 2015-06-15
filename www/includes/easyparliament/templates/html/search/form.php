1891f4fc5a5e74e3201463896df1a2734b4d9d8d
Struan Donald <struan@exo.org.uk>
2015-06-15 14:40:01 +0100

    escape strings in templates

--- www/includes/easyparliament/templates/html/search/form.php
+++ www/includes/easyparliament/templates/html/search/form.php
@@ -2,7 +2,7 @@
             <div class="search-page__section search-page__section--search">
                 <div class="search-page__section__primary">
                     <p class="search-page-main-inputs">
-                        <input type="text" name="s" value="<?= $search_keyword ?>" class="form-control">
+                        <input type="text" name="s" value="<?= _htmlentities($search_keyword) ?>" class="form-control">
                         <button type="submit" class="button">Search</button>
                     </p>
                     <?php if (isset($warnings) ) { ?>
@@ -14,11 +14,11 @@
                         <ul class="search-result-display-options">
                         <li><a href="#options" class="search-options-toggle js-toggle-search-options">Advanced search</a></li>
                         <?php if ( $is_adv ) { ?>
-                            <?= $search_from ? "<li>From: $search_from</li>" : '' ?>
-                            <?= $search_to ? "<li>To: $search_to</li>" : '' ?>
-                            <?= $search_person ? "<li>Person: $search_person</li>" : '' ?>
-                            <?= $search_section ? "<li>Section: $search_section_pretty</li>" : '' ?>
-                            <?= $search_column ? "<li>Column: $search_column</li>" : '' ?>
+                            <?= $search_from ? '<li>From: ' . _htmlentities($search_from) . '</li>' : '' ?>
+                            <?= $search_to ? '<li>To: ' . _htmlentities($search_to) . '</li>' : '' ?>
+                            <?= $search_person ? '<li>Person: ' . _htmlentities($search_person) . '</li>' : '' ?>
+                            <?= $search_section ? '<li>Section: ' . _htmlentities($search_section_pretty) . '</li>' : '' ?>
+                            <?= $search_column ? '<li>Column: ' . _htmlentities($search_column) . '</li>' : '' ?>
                         <?php } ?>
                         </ul>
                     </p>
@@ -32,9 +32,9 @@
                     <h4>Date range</h4>
                     <div class="search-option">
                         <div class="search-option__control search-option__control--date-range">
-                            <input name="from" type="date" value="<?= $search_from ?>" class="form-control">
+                            <input name="from" type="date" value="<?= _htmlentities($search_from) ?>" class="form-control">
                             <span>to</span>
-                            <input name="to" type="date" value="<?= $search_to ?>" class="form-control">
+                            <input name="to" type="date" value="<?= _htmlentities($search_to) ?>" class="form-control">
                         </div>
                         <div class="search-option__hint">
                             <p>You can give a <strong>start date, an end date, or both</strong> to restrict results to a particular date range. A missing end date implies the current date, and a missing start date implies the oldest date we have in the system. Dates can be entered in any format you wish, e.g. <strong>&ldquo;3rd March 2007&rdquo;</strong> or <strong>&ldquo;17/10/1989&rdquo;</strong></p>
@@ -44,7 +44,7 @@
                     <h4>Person</h4>
                     <div class="search-option">
                         <div class="search-option__control">
-                            <input name="person" type="text" value="<?= $search_person ?>" class="form-control">
+                            <input name="person" type="text" value="<?= _htmlentities($search_person) ?>" class="form-control">
                         </div>
                         <div class="search-option__hint">
                             <p>Enter a name here to restrict results to contributions only by that person.</p>
@@ -84,7 +84,7 @@
                     <h4>Column</h4>
                     <div class="search-option">
                         <div class="search-option__control">
-                            <input name="column" type="text" value="<?= $search_column ?>" class="form-control">
+                            <input name="column" type="text" value="<?= _htmlentities($search_column) ?>" class="form-control">
                         </div>
                         <div class="search-option__hint">
                             <p>If you know the actual Hansard column number of the information you are interested in (perhaps you&rsquo;re looking up a paper reference), you can restrict results to that.</p>
