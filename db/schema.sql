4f1a749b16360885440775a5752cd226ba6f5b68
Struan Donald <struan@exo.org.uk>
2015-07-01 17:20:58 +0100

    calculate and store party positions on policies
    Run over all policies and use the MPs division votes to calculate the
    parties position on the policy.
    Then store that position in a table for later lookup.

--- db/schema.sql
+++ db/schema.sql
@@ -159,6 +159,19 @@ CREATE TABLE `persondivisionvotes` (
   KEY `person_id` (`person_id`)
 );
 
+CREATE TABLE `partypolicy` (
+  `id` int(11) NOT NULL auto_increment,
+  `house` int(11) default NULL,
+  `party` varchar(100) collate latin1_spanish_ci NOT NULL default '',
+  `policy_id` varchar(100) NOT NULL default '',
+  `score` float NOT NULL default 0,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `party_policy` (`party`, `house`, `policy_id`),
+  KEY `party` (`party`),
+  KEY `policy_id` (`policy_id`)
+);
+ALTER TABLE `partypolicy` COLLATE latin1_spanish_ci;
+
 CREATE TABLE `moffice` (
   `moffice_id` varchar(100) NOT NULL,
   `dept` varchar(255) NOT NULL default '',
