ed571c2f417f6419e6bf50b77c399269d552eaad
Matthew Somerville <matthew-github@dracos.co.uk>
2015-07-20 17:27:39 +0100

    Add more progress lines, including timings.
    Remove empty output lines, unneeded package, no error if directories
    already exist, and run bundle install in the repo to make sure the
    precisely needed package versions are installed.

--- puphpet/files/exec-once/install.sh
+++ puphpet/files/exec-once/install.sh
@@ -20,49 +20,53 @@ sed -i 's/define ("DOMAIN", "www.example.org");/define ("DOMAIN", "twfy.mysociet
 sed -i 's/define ("COOKIEDOMAIN", "www.example.org");/define ("COOKIEDOMAIN", "twfy.mysociety");/g' /data/twfy/conf/general
 
 if ! dpkg -l php5-xapian 2>/dev/null | grep -q '^.i'; then
-    echo $DONE_MSG
     # Install php5-xapian from source
     # From http://trac.xapian.org/wiki/FAQ/PHP%20Bindings%20Package
-    echo "Installing php5-xapian from source..."
+    echo "[$(date +"%T")] Installing php5-xapian from source..."
     cd /tmp
-    echo -n "  Build dependencies for xapian-bindings... "
+    echo "  Build dependencies for xapian-bindings... "
     apt-get -qq build-dep xapian-bindings >/dev/null
-    echo $DONE_MSG
     echo "  PHP dev headers and devscripts... "
     apt-get -qq install php5-dev php5-cli devscripts >/dev/null
-    echo $DONE_MSG
-    echo -n "  xapian-bindings source... "
+    echo "  xapian-bindings source... "
     apt-get -qq source xapian-bindings >/dev/null
-    echo $DONE_MSG
     cd xapian-bindings-1.2.*
     rm -f debian/control debian/*-stamp
-    echo -n "  debian/rules maint with PHP_VERSIONS set... "
+    echo "  debian/rules maint with PHP_VERSIONS set... "
     env PHP_VERSIONS=5 debian/rules maint >/dev/null
-    echo $DONE_MSG
     sed -i 's!include_path=php5$!include_path=$(srcdir)/php5!' php/Makefile.in
     echo auto-commit >> debian/source/options
-    echo -n "  debuild... "
+    echo "  debuild... "
     # disable tests as they fail on PHP 5.4
     env DEB_BUILD_OPTIONS=nocheck debuild -e PHP_VERSIONS=5 -us -uc
-    echo $DONE_MSG
     cd ..
-    echo -n "  installing package... "
+    echo "  installing package... "
     dpkg -i php5-xapian_*.deb >/dev/null
     rm -rf *xapian*
 fi
 
-mkdir /data/parlparse
+echo "[$(date +"%T")] Cloning parlparse repository (member data and parsing code)"
+mkdir -p /data/parlparse
 git clone https://github.com/mysociety/parlparse.git /data/parlparse
 
-mkdir /data/searchdb
-mkdir /data/parldata
+mkdir -p /data/searchdb
+mkdir -p /data/parldata
 
+echo "[$(date +"%T")] Importing some debates data"
 /data/twfy/scripts/xml2db.pl --wrans --westminhall --debates --wms --lordsdebates --ni --scotland --scotwrans --scotqs --standing --all
+echo "[$(date +"%T")] Importing people"
 /data/twfy/scripts/load-people
+echo "[$(date +"%T")] Generating stats"
 /data/twfy/scripts/mpinfoin.pl
+echo "[$(date +"%T")] Fetching future data"
 cd /data/twfy/scripts
 python future-fetch.py
+echo "[$(date +"%T")] Indexing"
 perl /data/twfy/search/index.pl all
 
-cd /data/twfy/www/docs/style
-compass compile
+echo "[$(date +"%T")] Compiling CSS"
+cd /data/twfy
+# compass is already installed, but our Gemfile.lock versions won't match
+# exactly what was installed earlier in the process
+bundle install
+compass compile www/docs/style
