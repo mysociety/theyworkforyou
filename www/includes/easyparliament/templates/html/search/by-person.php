c51fd552001a76d00209c6c2787af4755e6a607e
Zarino Zappia <mail@zarino.co.uk>
2016-01-05 13:07:42 +0000

    Fix indenting in search page templates

--- www/includes/easyparliament/templates/html/search/by-person.php
+++ www/includes/easyparliament/templates/html/search/by-person.php
@@ -7,45 +7,79 @@
 
         <div class="search-page__section search-page__section--results">
             <div class="search-page__section__primary">
-            <h2>Who says <em class="current-search-term"><?= _htmlentities($searchstring) ?></em> the most?</h2>
+                <h2>Who says <em class="current-search-term"><?= _htmlentities($searchstring) ?></em> the most?</h2>
 
-            <?php if ( isset($error) ) {
-                if ( $error == 'No results' && isset( $house ) && $house != 0 ) { ?>
-                <ul class="search-result-display-options">
-                    <li>
-                        <?php if ( $house == 1 ) { ?>No results for MPs only<?php }
-                        else if ( $house == 2 ) { ?>No results for Peers only<?php }
-                        else if ( $house == 4 ) { ?>No results for MSPs only<?php }
-                        else if ( $house == 3 ) { ?>No results for MLAs only<?php } ?> |
-                        <a href="<?= $this_url->generate('html') ?>">Show results for all speakers</a></li>
-                </ul>
+              <?php if ( isset($error) ) { ?>
+                <?php if ( $error == 'No results' && isset( $house ) && $house != 0 ) { ?>
+                  <ul class="search-result-display-options">
+                      <li>
+                        <?php if ( $house == 1 ) { ?>
+                          No results for MPs only
+                        <?php } else if ( $house == 2 ) { ?>
+                          No results for Peers only
+                        <?php } else if ( $house == 4 ) { ?>
+                          No results for MSPs only
+                        <?php } else if ( $house == 3 ) { ?>
+                          No results for MLAs only
+                        <?php } ?>
+                          |
+                          <a href="<?= $this_url->generate('html') ?>">Show results for all speakers</a>
+                      </li>
+                  </ul>
                 <?php } else { ?>
-                <p class="search-results-legend"><?= $error ?></p>
+                  <p class="search-results-legend"><?= $error ?></p>
                 <?php } ?>
-            <?php } ?>
+              <?php } ?>
 
-            <?php if ( $wtt ) { ?>
+              <?php if ( $wtt ) { ?>
                 <p><strong>Now, try reading what a couple of these Lords are saying,
                 to help you find someone appropriate. When you've found someone,
                 hit the "I want to write to this Lord" button on their results page
                 to go back to WriteToThem.
                 </strong></p>
-            <?php } ?>
+              <?php } ?>
 
-            <?php if ( isset($speakers) && count($speakers) ) { ?>
-                <?php if ( !$wtt ) { ?>
-                <ul class="search-result-display-options">
-                    <li>Results grouped by person</li>
-                    <li><?php if ( $house == 0 ) { ?>Show All<?php } else { ?><a href="<?= $this_url->generate('html') ?>">Show All</a><?php } ?> |
-                        <?php if ( $house == 1 ) { ?>MPs only<?php } else { ?><a href="<?= $this_url->generate('html', array('house'=>1)) ?>">MPs only</a><?php } ?> |
-                        <?php if ( $house == 2 ) { ?>Peers only<?php } else { ?><a href="<?= $this_url->generate('html', array('house'=>2)) ?>">Lords only</a><?php } ?> |
-                        <?php if ( $house == 4 ) { ?>MSPs only<?php } else { ?><a href="<?= $this_url->generate('html', array('house'=>4)) ?>">MSPs only</a><?php } ?> |
-                        <?php if ( $house == 3 ) { ?>MLAs only<?php } else { ?><a href="<?= $this_url->generate('html', array('house'=>3)) ?>">MLAs only</a><?php } ?></li>
-                    <li><a href="<?= $ungrouped_url->generate() ?>">Ungroup results</a></li>
-                </ul>
+              <?php if ( isset($speakers) && count($speakers) ) { ?>
 
-                <p class="search-results-legend">The <?= isset($limit_reached) ? '5000 ' : '' ?>most recent mentions of the exact phrase <em class="current-search-term"><?= _htmlentities($searchstring) ?></em>, grouped by speaker name.</p>
-                <?php } ?>
+                  <?php if ( !$wtt ) { ?>
+                    <ul class="search-result-display-options">
+                        <li>Results grouped by person</li>
+                        <li>
+                          <?php if ( $house == 0 ) { ?>
+                            Show All
+                          <?php } else { ?>
+                            <a href="<?= $this_url->generate('html') ?>">Show All</a>
+                          <?php } ?>
+                            |
+                          <?php if ( $house == 1 ) { ?>
+                            MPs only
+                          <?php } else { ?>
+                            <a href="<?= $this_url->generate('html', array('house'=>1)) ?>">MPs only</a>
+                          <?php } ?>
+                            |
+                          <?php if ( $house == 2 ) { ?>
+                            Peers only
+                          <?php } else { ?>
+                            <a href="<?= $this_url->generate('html', array('house'=>2)) ?>">Lords only</a>
+                          <?php } ?>
+                            |
+                          <?php if ( $house == 4 ) { ?>
+                            MSPs only
+                          <?php } else { ?>
+                            <a href="<?= $this_url->generate('html', array('house'=>4)) ?>">MSPs only</a>
+                          <?php } ?>
+                            |
+                          <?php if ( $house == 3 ) { ?>
+                            MLAs only
+                          <?php } else { ?>
+                            <a href="<?= $this_url->generate('html', array('house'=>3)) ?>">MLAs only</a>
+                          <?php } ?>
+                        </li>
+                        <li><a href="<?= $ungrouped_url->generate() ?>">Ungroup results</a></li>
+                    </ul>
+
+                    <p class="search-results-legend">The <?= isset($limit_reached) ? '5000 ' : '' ?>most recent mentions of the exact phrase <em class="current-search-term"><?= _htmlentities($searchstring) ?></em>, grouped by speaker name.</p>
+                  <?php } ?>
 
                 <table class="search-results-grouped">
                     <thead>
@@ -56,41 +90,44 @@
                         </tr>
                     </thead>
                     <tbody>
-                    <?php foreach ( $speakers as $pid => $speaker ) { ?>
+                      <?php foreach ( $speakers as $pid => $speaker ) { ?>
+
                         <?php if ( $wtt && $pid == 0 ) { continue; } // skip heading count for WTT lords list ?>
+
                         <tr>
                             <td><?= $speaker['count'] ?></td>
                             <td>
-                                <?php if ( $pid ) { ?>
-                                    <?php if ( !$wtt || $speaker['left'] == '9999-12-31' ) { ?>
-                                    <a href="/search/?q=<?= _htmlentities($searchstring) ?>&amp;pid=<?= $pid ?><?= isset($wtt) && $speaker['left'] == '9999-12-31' ? '&amp;wtt=2' : '' ?>">
-                                    <?php } ?>
-                                    <?= isset($speaker['name']) ? $speaker['name'] : 'N/A' ?>
-                                    <?php if ( !$wtt || $speaker['left'] == '9999-12-31' ) { ?>
-                                    </a>
-                                    <?php } ?>
+                              <?php if ( $pid ) { ?>
+                                <?php if ( !$wtt || $speaker['left'] == '9999-12-31' ) { ?>
+                                  <a href="/search/?q=<?= _htmlentities($searchstring) ?>&amp;pid=<?= $pid ?><?= isset($wtt) && $speaker['left'] == '9999-12-31' ? '&amp;wtt=2' : '' ?>">
+                                <?php } ?>
+                                <?= isset($speaker['name']) ? $speaker['name'] : 'N/A' ?>
+                                <?php if ( !$wtt || $speaker['left'] == '9999-12-31' ) { ?>
+                                  </a>
+                                <?php } ?>
                                 <?php if ( isset($speaker['party']) ) { ?>
-                                <span class="search-results-grouped__speaker-party">(<?= $speaker['party'] ?>)</span>
+                                  <span class="search-results-grouped__speaker-party">(<?= $speaker['party'] ?>)</span>
                                 <?php } ?>
                                 <?php if ( $house != 2 ) { ?>
-                                <?= isset($speaker['office']) ? ' - ' . join('; ', $speaker['office']) : '' ?>
+                                  <?= isset($speaker['office']) ? ' - ' . join('; ', $speaker['office']) : '' ?>
                                 <?php } ?>
-                                <?php } else { ?>
+                              <?php } else { // no $pid ?>
                                 <?= $speaker['name'] ?>
-                                <?php } ?>
+                              <?php } ?>
                             </td>
                             <td>
-                            <?php if ( format_date($speaker['pmindate'], 'M Y') == format_date($speaker['pmaxdate'], 'M Y') ) { ?>
-                            <?= format_date($speaker['pmindate'], 'M Y') ?>
-                            <?php } else { ?>
-                            <?= format_date($speaker['pmindate'], 'M Y') ?>&nbsp;&ndash;&nbsp;<?= format_date($speaker['pmaxdate'], 'M Y') ?>
-                            <?php } ?>
+                              <?php if ( format_date($speaker['pmindate'], 'M Y') == format_date($speaker['pmaxdate'], 'M Y') ) { ?>
+                                <?= format_date($speaker['pmindate'], 'M Y') ?>
+                              <?php } else { ?>
+                                <?= format_date($speaker['pmindate'], 'M Y') ?>&nbsp;&ndash;&nbsp;<?= format_date($speaker['pmaxdate'], 'M Y') ?>
+                              <?php } ?>
                             </td>
                         </tr>
-                    <?php } ?>
+                      <?php } ?>
                     </tbody>
                 </table>
-            <?php } ?>
+              <?php } // end of isset($speakers) ?>
+
             </div>
 
             <?php include 'sidebar.php' ?>
