b29c7d9f819934858a361bec3c23f954fdb53daf
Struan Donald <struan@exo.org.uk>
2016-02-19 12:31:40 +0000

    Update contact page text
    Fixes #901

--- www/includes/easyparliament/templates/html/static/contact.php
+++ www/includes/easyparliament/templates/html/static/contact.php
@@ -3,7 +3,7 @@
         <div class="panel">
             <h1>Who do you want to contact?</h1>
 
-            <h2>Contact your MP or MLA</h2>
+            <h2>Contact your MP, MSP or MLA</h2>
 
             <p>Use <a href="http://www.writetothem.com">WriteToThem</a> to contact your MP or MLA.</p>
 
@@ -11,20 +11,32 @@
             <p>Use <a href="http://www.writetothem.com/lords">WriteToThem Lords</a> to contact a lord.</p>
 
             <h2>Contact the TheyWorkForYou team</h2>
-            <p>
-            Please tell us what you think about <strong>TheyWorkForYou.com</strong>
-            </p>
-            <ul>
-            <li>Did it work?</li>
-            <li>Do you like it? </li>
-            <li>How can we improve it? </li>
-            </ul>
-            <p>
-            We seek responses to all these questions.
-            </p>
-            <p><strong>We are not your MP</strong>. To contact your MP <a href="http://www.writetothem.com">click here</a>. The email address of TheyWorkForYou.com, which is run by <strong>a charity</strong>, is: <a href="mailto:<?php echo str_replace('@', '&#64;', CONTACTEMAIL); ?>"><?php echo str_replace('@', '&#64;', CONTACTEMAIL); ?></a></p>
 
-            <p>If you are a member of the press you can get in touch at <a href="mailto:press@mysociety.org">press@mysociety.org</a>, or read more in our <a href="http://www.mysociety.org/press-area/theyworkforyou/">press area</a>.</p>
+            <p>
+            Tell us about any problems you've had with the site &mdash; but first, check our
+            <a href="/help/">FAQs</a> to see if your answer is there.
+            </p>
+
+            <p>
+            It probably is: we reckon they cover about 90% of our normal postbag &mdash; so
+            give them a read, and you'll likely save yourself some time.
+            </p>
+
+            <p>
+            Also: please remember that <strong>we are not your MP</strong>.<br>
+            But we do provide a nifty service through which you can contact your MP: <a title="WriteToThem" href="https://www.writetothem.com">click here</a>.
+            </p>
+
+            <p>
+            Right! You've read the FAQs and you definitely want to contact TheyWorkForYou's
+            technical support team? And not your MP? Good. You can mail us on
+            <a href="mailto:support@theyworkforyou.com">support@theyworkforyou.com</a>
+            </p>
+
+            <p>
+            If you are a journalist you might find what you need <a href="https://www.mysociety.org/press/">in our press area</a>. Or you can
+            get in touch with our press contact via <a href="mailto:press@mysociety.org">press@mysociety.org</a>.
+            </p>
         </div>
     </div>
 </div>
