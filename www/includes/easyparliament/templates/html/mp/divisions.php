7fbcab4c7bc580eef547071d469169a910051f51
Struan Donald <struan@exo.org.uk>
2016-03-09 14:59:45 +0000

    link to relevant FAQ entries from votes and divions page
    Fixes #899

--- www/includes/easyparliament/templates/html/mp/divisions.php
+++ www/includes/easyparliament/templates/html/mp/divisions.php
@@ -144,6 +144,10 @@ include_once INCLUDESPATH . "easyparliament/templates/html/mp/header.php";
                 <div class="panel">
                     <p>Note for journalists and researchers: The data on this page may be used freely,
                        on condition that TheyWorkForYou.com is cited as the source.</p>
+
+                    <p>For an explanation of the vote descriptions please see the FAQ entries on
+                    <a href="/help/#vote-descriptions">vote descriptions</a> and
+                    <a href="/help/#votingrecord">how the voting record is decided</a></p>
                 </div>
 
             </div>
