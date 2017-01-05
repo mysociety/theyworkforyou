b95390b496014849de54b978e359ecf2b0c52800
Struan Donald <struan@exo.org.uk>
2017-01-05 17:27:53 +0000

    small formatting improvements to house rules page
    Fixes #1182

--- www/includes/easyparliament/templates/html/static/houserules.php
+++ www/includes/easyparliament/templates/html/static/houserules.php
@@ -7,9 +7,9 @@
 <p>But as we all know, with great power comes great responsibility. In order to keep TheyWorkForYou useful and relevant to everyone, there are a few rules. They are important.</p>
 
 <p>Stick to these and you can&rsquo;t go wrong. Transgress once, and we&rsquo;ll send you a warning. Do it again and you&rsquo;re banned, and that&rsquo;s as fair as we can make it.</p>
-            
+
             <h2>Annotations should be</h2>
-            
+
             <ul><li><b>Relevant to the debate you are commenting on</b>: that&rsquo;s <em>completely</em> relevant.</li>
                 <li><b>Factual</b>: Check your sources. Citing them is good.</li>
                 <li><b>Non-partisan</b>, ie not showing a political bias</li>
@@ -18,10 +18,10 @@
 
             <h2>You might like to</h2>
 
-            <ul><li><b>Provide a link to a webpage</b> &mdash; but that page must also be relevant, factual, useful, and non-partisan.   </li></ul>    
-  
+            <ul><li><b>Provide a link to a webpage</b> &mdash; but that page must also be relevant, factual, useful, and non-partisan.   </li></ul>
+
             <h2>Things to avoid</h2>
-   
+
             <ul><li><b>Don't go off-topic</b>. You&rsquo;re commenting on the debate, nothing else.</li>
                 <li><b>We don't want your opinions or anecdotes</b>. There are plenty of other forums for those: try your local newspaper, for example.</li>
                 <li><b>Avoid bad or upsetting language</b>. All types of people use TheyWorkForYou. Don&rsquo;t write anything you wouldn&rsquo;t want your own grandmother or child to read &mdash; because they (or someone just like them) might.</li>
@@ -31,12 +31,16 @@
 
             <h2>If you see an annotation which does not adhere to these rules</h2>
 
+            <p>
             <b>You can report it</b>. You&rsquo;ll see a small link marked &lsquo;<em>report this annotation</em>&rsquo; at the top of the comment, near the timestamp.
 
-We&rsquo;ll take a look at the annotation and take any necessary steps.
+            We&rsquo;ll take a look at the annotation and take any necessary steps.
+            </p>
 
-            <b>Thank you for helping us keep TheyWorkForYou helpful, friendly and safe.</b>   
-           
+            <p>
+            <b>Thank you for helping us keep TheyWorkForYou helpful, friendly and safe.</b>
+            </p>
+
         </div>
     </div>
 </div>
