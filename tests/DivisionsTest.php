09f69597b9b9c618d68c5d2b5015329ac35c559f
Struan Donald <struan@exo.org.uk>
2017-02-27 14:48:27 +0000

    tests for MP recent votes page

--- tests/DivisionsTest.php
+++ tests/DivisionsTest.php
@@ -20,6 +20,12 @@ class DivisionsTest extends FetchPageTestCase
         return $this->fetch_page( array( 'pagetype' => 'divisions', 'pid' => 2, 'policy' => 363, 'url' => '/mp/2/test_current-mp/test_westminster_constituency/divisions' ) );
     }
 
+    private function fetch_recent_page()
+    {
+        $vars = array( 'pagetype' => 'recent', 'pid' => 2, 'url' => '/mp/2/test_current-mp/test_westminster_constituency/recent' );
+        return $this->base_fetch_page($vars, 'mp', 'index.php', '/mp/recent.php');
+    }
+
     public function testSinglePolicy() {
         $p = new MySociety\TheyWorkForYou\Policies;
         $this->assertEquals(count($p->getArray()), 92);
@@ -104,5 +110,11 @@ class DivisionsTest extends FetchPageTestCase
     public function testNotEnoughInfoStatement() {
         return $this->fetch_page( array( 'pagetype' => 'divisions', 'pid' => 2, 'policy' => 810, 'url' => '/mp/2/test_current-mp/test_westminster_constituency/divisions' ) );
         $this->assertContains('we don&rsquo;t have enough information to calculate Test Current-MP&rsquo;s position', $page);
+    }
+
+    public function testRecentDivisions() {
+        $page = $this->fetch_recent_page();
+        $this->assertContains('<li id="pw-2013-01-01-3-commons" class="policy-vote--major', $page);
+        $this->assertNotContains('<li id="pw-2012-01-01-13-commons" class="policy-vote--major', $page);
     }
 }
