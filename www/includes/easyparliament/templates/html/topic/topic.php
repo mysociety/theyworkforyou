411732e6fe25099a2136bfec13d5c4160045c2c7
Zarino Zappia <mail@zarino.co.uk>
2017-03-27 15:24:18 +0100

    New markup for Topic page

--- www/includes/easyparliament/templates/html/topic/topic.php
+++ www/includes/easyparliament/templates/html/topic/topic.php
@@ -3,27 +3,9 @@
         <div class="full-page__row">
 
             <div class="topic-name">
-                <h1><?= $title ?></h1>
-                <h1 class="subheader">&amp; the UK Parliament</h1>
-                <p class="lead"><?= $description ?> Here are some places you might want to start.</p>
+                <h1><?= _htmlspecialchars($title) ?></h1>
+                <p class="lead"><?= _htmlspecialchars($description) ?></p>
             </div>
-
-          <?php if ($display_postcode_form): ?>
-            <div class="topic-postcode-search">
-                <h3>What does your MP think?</h3>
-
-                <form action="#yourrep" method="get">
-                    <div class="row collapse">
-                        <div class="small-10 columns">
-                            <input type="text" name="pc" value="" maxlength="10" size="10" placeholder="Your postcode">
-                        </div>
-                        <div class="small-2 columns">
-                            <input type="submit" value="GO" class="button prefix">
-                        </div>
-                    </div>
-                </form>
-            </div>
-          <?php endif; ?>
 
         </div>
     </div>
@@ -31,150 +13,98 @@
 
 <div class="full-page">
     <div class="full-page__row">
-        <div class="topic-content">
 
-            <?php if (count($actions) > 0) { ?>
-            <div class="topic-block">
+        <div class="small-12 large-3 large-push-9 columns">
+          <?php if ($display_postcode_form): ?>
+            <div class="topic-sidebar topic-postcode-search">
+                <h3>How did your MP vote?</h3>
+                <form action="#yourrep" method="get">
+                    <label for="pc">Your postcode</label>
+                    <input type="text" name="pc" id="pc" value="" maxlength="10" size="10" placeholder="eg. TF1 9QP">
+                    <input type="submit" value="GO" class="button prefix">
+                </form>
+            </div>
+          <?php endif; ?>
 
+            <div class="topic-sidebar">
+              <h3>
+                  Get notifications about
+                  <strong><?= _htmlspecialchars($title) ?></strong>
+              </h3>
+              <a href="#" class="button expand">Set up an alert</a>
+            </div>
+        </div>
 
-                <ul class="large-block-grid-2">
+        <div class="topic-content small-12 large-9 large-pull-3 columns">
 
-                <?php foreach ($actions as $action): ?>
+          <?php if (isset($positions)): ?>
+            <div class="topic-block policies">
+                <h2 id="yourrep">How <?= $member_name ?> voted on <?= $title ?></h2>
 
-                    <li>
+              <?php if ($total_votes == 0): ?>
+                <p>
+                    <a href="<?= $member_url ?>"><?= $member_name ?></a> hasn't
+                    voted on any of the key issues on <?= _htmlspecialchars($title) ?>. You may want
+                    to <a href="<?= $member_url ?>/votes">see all their votes</a>.
+                </p>
 
-                        <div class="panel">
+              <?php else: ?>
+                <ul class="vote-descriptions">
+                  <?php
+                  $policy_ids = array();
 
-                            <div class="row">
+                  foreach ($positions as $position) {
+                      if (!in_array($position['policy_id'], $policy_ids)) {
+                          $description = ucfirst($position['desc']);
+                          $link = sprintf(
+                              '%s/divisions?policy=%s',
+                              $member_url,
+                              $position['policy_id']
+                          );
+                          $show_link = $position['position'] != 'has never voted on';
+                          $key_vote = $position;
+                          include(dirname(__DIR__) . '/mp/_vote_description.php');
 
-                                <div class="medium-3 columns show-for-medium-up">
-
-                                    <img src="/images/icons/topic-<?= $action['icon'] ?>.png">
-
-                                </div>
-
-                                <div class="medium-9 columns">
-
-                                    <h3><a href="<?= $action['href'] ?>"><?= $action['title'] ?></a></h3>
-
-                                    <p><?= $action['description'] ?></p>
-
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                    </li>
-
-                <?php endforeach; ?>
-
+                          $policy_ids[] = $position['policy_id'];
+                      }
+                  } ?>
                 </ul>
 
+              <?php endif; ?>
             </div>
-            <?php } ?>
+          <?php endif; ?>
 
-            <?php if (isset($recent_divisions)): ?>
-                <div class="topic-block policies">
-                    <div class="row">
-                        <div class="medium-12 columns unpad-left">
-                            <h2>Recent Votes</h2>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <ul class="policies-list">
-                        <?php foreach ($recent_divisions as $position) { ?>
-                        <li id="<?= $position['division_id'] ?>">The majority of MPs <?= $position['text'] ?> <small>(<?= format_date($position['date'], SHORTDATEFORMAT) ?>)</small> <a class="dream_details" href="<?= $position['url'] ?>">Show debate</a></li>
-                        <?php } ?>
-                        </ul>
-                    </div>
-                </div>
-            <?php endif; ?>
+          <?php if (isset($recent_divisions)): ?>
+            <div class="topic-block policies">
+                <h2>Recent votes on <?= _htmlspecialchars($title) ?></h2>
+                <ul class="vote-descriptions">
+                  <?php foreach ($recent_divisions as $position) { ?>
+                    <li id="<?= $position['division_id'] ?>">
+                        The majority of MPs <?= $position['text'] ?>
+                        <a class="vote-description__source" href="<?= $position['url'] ?>">
+                            Show debate
+                        </a>
+                        <a class="vote-description__evidence" href="<?= $position['url'] ?>">
+                            <?= format_date($position['date'], SHORTDATEFORMAT) ?>
+                        </a>
+                    </li>
+                  <?php } ?>
+                </ul>
+            </div>
+          <?php endif; ?>
 
-
-            <?php if (isset($positions)): ?>
-
-                <div class="topic-block policies">
-
-                    <div class="row">
-
-                        <div class="medium-8 columns unpad-left">
-
-                            <h2 id="yourrep">Your Representative</h2>
-
-                            <?php if ($display_postcode_form): ?>
-
-                            <p>Find our how your representative has voted on key issues around <?= $policytitle ?>.</p>
-
-                        </div>
-
-                        <div class="medium-4 columns topic-postcode-search">
-
-                            <h3>What does your MP think?</h3>
-
-                            <form action="#yourrep" method="get">
-
-                                <div class="row collapse">
-                                    <div class="small-10 columns">
-                                        <input type="text" name="pc" value="" maxlength="10" size="10" placeholder="Your postcode">
-                                    </div>
-                                    <div class="small-2 columns">
-                                        <input type="submit" value="GO" class="button prefix">
-                                    </div>
-                                </div>
-                            </form>
-
-                        </div>
-
-                            <?php endif; ?>
-
-                            <?php if (isset($member_name)): ?>
-
-
-                        </div>
-
-                        <div class="medium-4 columns topic-mp-info show-for-medium-up">
-                            <div class="row">
-                                <div class="small-3 columns">
-                                    <img src="<?= $member_image['url'] ?>">
-                                </div>
-                                <div class="small-9 columns">
-                                    <h3><?= $member_name ?></h3>
-                                    <p><?= $member_constituency ?></p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="row">
-                        <div class="medium-12 columns unpad-left">
-                            <?php if ($total_votes == 0): ?>
-
-                            <p><a href="<?= $member_url ?>"><?= $member_name ?></a> hasn't voted on any of the key issues on <?= $title ?>. You may want to <a href="<?= $member_url ?>/votes">see all their votes</a>.</p>
-                            <?php else: ?>
-                            <ul class="policies-list">
-                            <?php $policy_ids = array(); ?>
-                            <?php foreach ($positions as $position) { ?>
-
-                                <?php if (!in_array($position['policy_id'], $policy_ids)) { ?>
-                                <li><?= ucfirst($position['desc']) ?><a class="dream_details" href="http://www.publicwhip.org.uk/mp.php?mpid=<?= $member_id ?>&dmp=<?= $position['policy_id'] ?>">Show votes</a></li>
-
-                                <?php $policy_ids[] = $position['policy_id']; ?>
-                                <?php } ?>
-
-                            <?php } ?>
-                            </ul>
-
-                            <?php endif; ?>
-
-                            <?php endif; ?>
-                            <?php endif; ?>
-
-                    </div>
-
-                </div>
-
+          <?php if (count($business) > 0): ?>
+            <div class="topic-block policies">
+                <h2>Parliamentary business about <?= _htmlspecialchars($title) ?></h2>
+                <ul class="business-list">
+                  <?php foreach ($business as $item): ?>
+                    <li>
+                        <?php include(dirname(__DIR__) . '/section/_business_list_item.php'); ?>
+                    </li>
+                  <?php endforeach; ?>
+                </ul>
+            </div>
+          <?php endif; ?>
 
         </div>
     </div>
