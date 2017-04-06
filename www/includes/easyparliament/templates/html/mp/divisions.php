bbdfab6d984a0365392f702247f9cf00bd857fbb
Struan Donald <struan@exo.org.uk>
2017-04-06 16:10:12 +0100

    extract common vote footer text out to partial

--- www/includes/easyparliament/templates/html/mp/divisions.php
+++ www/includes/easyparliament/templates/html/mp/divisions.php
@@ -130,18 +130,8 @@ include_once INCLUDESPATH . "easyparliament/templates/html/mp/header.php";
                         <p>This person has not voted on this policy.</p>
                     </div>
 
-                <?php } ?>
-
-
-
-                <div class="panel">
-                    <p>Note for journalists and researchers: The data on this page may be used freely,
-                       on condition that TheyWorkForYou.com is cited as the source.</p>
-
-                    <p>For an explanation of the vote descriptions please see the FAQ entries on
-                    <a href="/help/#vote-descriptions">vote descriptions</a> and
-                    <a href="/help/#votingrecord">how the voting record is decided</a></p>
-                </div>
+                <?php }
+                include('_vote_footer.php'); ?>
 
             </div>
         </div>
