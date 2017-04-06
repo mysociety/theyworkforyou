c8fd0bdfa61c53a1c6e9825a1ae14b71f0931b9b
Struan Donald <struan@exo.org.uk>
2017-04-06 16:09:15 +0100

    extract common division template code to partial

--- www/includes/easyparliament/templates/html/mp/divisions.php
+++ www/includes/easyparliament/templates/html/mp/divisions.php
@@ -91,18 +91,10 @@ include_once INCLUDESPATH . "easyparliament/templates/html/mp/header.php";
                                             $show_all = TRUE;
                                         }
                                     ?>
-                                    <?php foreach ($policy['divisions'] as $division) { ?>
-                                        <li id="<?= $division['division_id'] ?>" class="<?= $division['strong'] || $show_all ? 'policy-vote--major' : 'policy-vote--minor' ?>">
-                                            <span class="policy-vote__date">On <?= strftime('%e %b %Y', strtotime($division['date'])) ?>:</span>
-                                            <span class="policy-vote__text"><?= $full_name ?><?= $division['text'] ?></span>
-                                            <?php if ( $division['url'] ) { ?>
-                                                <a class="vote-description__source" href="<?= $division['url'] ?>">Show full debate</a>
-                                            <?php } ?>
-                                        </li>
-
-                                    <?php $displayed_votes = TRUE; ?>
-
-                                    <?php } ?>
+                                    <?php foreach ($policy['divisions'] as $division) {
+                                        include('_division_description.php');
+                                        $displayed_votes = TRUE;
+                                    } ?>
                                     </ul>
 
                                     <div class="policy-votes-list-footer">
--- www/includes/easyparliament/templates/html/mp/recent.php
+++ www/includes/easyparliament/templates/html/mp/recent.php
@@ -44,14 +44,9 @@ include_once INCLUDESPATH . "easyparliament/templates/html/mp/header.php";
                           <div class="panel" id="<?= strftime('%Y-%m-%d', strtotime($division['date'])) ?>" data-magellan-destination="<?= strftime('%Y-%m-%d', strtotime($division['date'])) ?>">
                             <h2><?= strftime('%e %b %Y', strtotime($division['date'])) ?></h2>
                              <ul class="vote-descriptions policy-votes">
-                          <?php } ?>
-                          <li id="<?= $division['division_id'] ?>" class="<?= $division['strong'] || $show_all ? 'policy-vote--major' : 'policy-vote--minor' ?>">
-                              <span class="policy-vote__text"><?= $full_name ?><?= $division['text'] ?></span>
-                              <?php if ( $division['url'] ) { ?>
-                                  <a class="vote-description__source" href="<?= $division['url'] ?>">Show full debate</a>
-                              <?php } ?>
-                          </li>
-                        <?php }
+                          <?php }
+                          include('_division_description.php');
+                        }
                         echo('</div>');
                     }
                 } ?>
