ed4800031aa3e9a0e04b419d3bd0797b63f7e85a
Struan Donald <struan@exo.org.uk>
2017-05-10 12:01:44 +0100

    simplify logic on divisions page
    Bail out early if we are missing things and don't check for things that
    are definitely going to be there.

--- www/docs/divisions/division.php
+++ www/docs/divisions/division.php
@@ -5,6 +5,13 @@ include_once INCLUDESPATH . 'easyparliament/member.php';
 
 $this_page = 'divisions_vote';
 
+$vote = get_http_var('vote');
+
+if (!$vote) {
+    $PAGE->error_message("No vote specified", true);
+    exit();
+}
+
 $main_vote_mp = false;
 if ($mp = get_http_var('p')) {
     $MEMBER = new MySociety\TheyWorkForYou\Member(array('person_id' => $mp, 'house' => HOUSE_TYPE_COMMONS));
@@ -13,48 +20,42 @@ if ($mp = get_http_var('p')) {
     $MEMBER = new MySociety\TheyWorkForYou\Member(array('postcode' => $THEUSER->postcode(), 'house' => HOUSE_TYPE_COMMONS));
 }
 
-$vote = get_http_var('vote');
+$divisions = new MySociety\TheyWorkForYou\Divisions();
+$data = array('division' => $divisions->getDivisionResults($vote));
 
-if ($vote) {
-  $divisions = new MySociety\TheyWorkForYou\Divisions();
-  $data = array('division' => $divisions->getDivisionResults($vote));
-  if ($data['division']) {
-    $template = 'divisions/vote';
-    $DATA->set_page_metadata($this_page, 'title', $data['division']['division_title']);
-
-    $data['main_vote_mp'] = $main_vote_mp;
-
-    if (isset($MEMBER)) {
-        $data['member'] = $MEMBER;
-        $mp_vote = $divisions->getDivisionResultsForMember($vote, $MEMBER->person_id());
-        if ($mp_vote) {
-            $data['mp_vote'] = $mp_vote;
-            $data['mp_vote']['with_majority'] = false;
-            if ($data['mp_vote']['vote'] == $data['division']['vote']) {
-                $data['mp_vote']['with_majority'] = true;
-            }
-        } else {
-          if ($data['division']['date'] < $MEMBER->entered_house(1)['date']) {
-              $data['before_mp'] = true;
-          } else if ($data['division']['date'] > $MEMBER->left_house(1)['date']) {
-              $data['after_mp'] = true;
-          }
-        }
-    }
-
-    if ($data) {
-        $data['debate_time_human'] = False;
-        $data['debate_day_human'] = format_date($data['division']['date'], LONGDATEFORMAT);
-        $data['col_country'] = 'UK';
-        $data['location'] = 'in the House of Commons';
-        $data['current_assembly'] = 'uk-commons';
-        $data['nextprev'] = array('up' => array('url' => '/divisions/', 'body' => 'Recent Votes'));
-
-        MySociety\TheyWorkForYou\Renderer::output($template, $data);
-    }
-  } else {
-      $PAGE->error_message("Vote not found", true);
-  }
-} else {
-    $PAGE->error_message("No vote specified", true);
+if (!$data['division']) {
+    $PAGE->error_message("Vote not found", true);
+    exit();
 }
+
+$template = 'divisions/vote';
+$DATA->set_page_metadata($this_page, 'title', $data['division']['division_title']);
+
+$data['main_vote_mp'] = $main_vote_mp;
+
+if (isset($MEMBER)) {
+    $data['member'] = $MEMBER;
+    $mp_vote = $divisions->getDivisionResultsForMember($vote, $MEMBER->person_id());
+    if ($mp_vote) {
+        $data['mp_vote'] = $mp_vote;
+        $data['mp_vote']['with_majority'] = false;
+        if ($data['mp_vote']['vote'] == $data['division']['vote']) {
+            $data['mp_vote']['with_majority'] = true;
+        }
+    } else {
+      if ($data['division']['date'] < $MEMBER->entered_house(1)['date']) {
+          $data['before_mp'] = true;
+      } else if ($data['division']['date'] > $MEMBER->left_house(1)['date']) {
+          $data['after_mp'] = true;
+      }
+    }
+}
+
+$data['debate_time_human'] = False;
+$data['debate_day_human'] = format_date($data['division']['date'], LONGDATEFORMAT);
+$data['col_country'] = 'UK';
+$data['location'] = 'in the House of Commons';
+$data['current_assembly'] = 'uk-commons';
+$data['nextprev'] = array('up' => array('url' => '/divisions/', 'body' => 'Recent Votes'));
+
+MySociety\TheyWorkForYou\Renderer::output($template, $data);
