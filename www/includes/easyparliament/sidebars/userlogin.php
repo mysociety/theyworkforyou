18409d5139c8b6a77b605d72f19a592b491d7db5
Zarino Zappia <mail@zarino.co.uk>
2017-06-09 10:43:21 +0100

    Update message shown on login page sidebar
    Fixes #1261.

--- www/includes/easyparliament/sidebars/userlogin.php
+++ www/includes/easyparliament/sidebars/userlogin.php
@@ -3,7 +3,7 @@
 
 $this->block_start(array('id'=>'help', 'title'=>"Why do I need to sign in?"));
 ?>
-<p>You don't need to sign-in to browse or search the site. We only ask you to register and log-in if you want to add annotations or contribute to the glossary.</p>
+<p>Logging in allows you to manage your email alerts, and change your password or email address.</p>
 
 <p><strong>* If this box is checked, your login details will be stored on this computer.</strong> If you share this computer with other people, uncheck the box.</p>
 
