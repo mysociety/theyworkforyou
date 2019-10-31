72189ecf0d69d7c762a8952914e787f2895d511f
ryan.bibby <ryan.bibby@ukfast.co.uk>
2019-10-31 09:13:33 +0000

    Fall back to 'Show votes' if no link text is set
    Fixes #1480

--- www/includes/easyparliament/templates/html/mp/_vote_description.php
+++ www/includes/easyparliament/templates/html/mp/_vote_description.php
@@ -1,7 +1,7 @@
 <li>
     <?= $description ?>
     <?php if ( $show_link ) { ?>
-        <a class="vote-description__source" href="<?= $link ?>"><?= $link_text ?></a>
+        <a class="vote-description__source" href="<?= $link ?>"><?= isset($link_text) ? $link_text : 'Show votes' ?></a>
         <?php if (isset($key_vote)) { ?>
         <a class="vote-description__evidence" href="<?= $link ?>"><?= $key_vote['summary'] ?></a>
         <?php } ?>
