d09fe3138242faba64f2a444d860297a87cb0312
M Somerville <matthew-github@dracos.co.uk>
2020-03-05 11:38:50 +0000

    Remove GA cookies.

--- www/includes/easyparliament/templates/html/header.php
+++ www/includes/easyparliament/templates/html/header.php
@@ -71,7 +71,7 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-660910-1');  // Replace with your property ID.
+        ga('create', 'UA-660910-1', {'storage':'none'});
         ga('set', 'anonymizeIp', true);
         ga('send', 'pageview');
 
