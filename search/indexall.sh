2f12e9c2e468e8b9b927acdcdb82874fd2094b01
Sam Pearson <sam@sgp.me.uk>
2020-12-22 15:53:06 +0000

    Update the script used to generate a fresh Xapian index
    This will now index everything between 1919 and 2020 in chunks,
    printing progress and times as it goes.
    
    The script now relies on Bash 4 and greater's brace expansion, so
    it tests for that.

--- search/indexall.sh
+++ search/indexall.sh
@@ -1,33 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
 
-if [ -z "$1" ]
-then
-	echo "Please specify database as first parameter"
-	exit
+if [ "${BASH_VERSINFO:-0}" -lt 4 ] ; then
+    echo "Bash version 4 or greater is required."
+    exit 1
 fi
 
+echo "Starting complete index task at $(date)."
+
 # Do it in bits, as Perl and MySQL sometimes can't cope with it all in one go.
-for YEAR in 1999
-do
-	for MONTH in 11 12
-	do
-		./index.pl $1 daterange $YEAR-$MONTH-01 $YEAR-$MONTH-31
-	done
-done
-for YEAR in 2000 2001 2002 2003 2004 2005 2006 2007
-do
-	for MONTH in 01 02 03 04 05 06 07 08 09 10 11 12
-	do
-		./index.pl $1 daterange $YEAR-$MONTH-01 $YEAR-$MONTH-31
-	done
-done
-for YEAR in 2008
-do
-	for MONTH in 01 02
-	do
-		./index.pl $1 daterange $YEAR-$MONTH-01 $YEAR-$MONTH-31
-	done
+for YEAR in {1919..2020} ; do
+
+    # Bash 4+ will do the right thing with the leading zero.
+    for MONTH in {01..12} ; do
+        echo "Indexing ${YEAR}-${MONTH} at $(date)..."
+        ./index.pl daterange ${YEAR}-${MONTH}-01 ${YEAR}-${MONTH}-31
+    done
+
 done
 
-./index.pl $1 check
+echo "Checking index at $(date)..."
+./index.pl check
+
+echo "Finished at $(date)."
