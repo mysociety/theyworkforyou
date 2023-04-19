b7607b9ed962b96f8968459150cb46114966d934
Alex Parsons <alex.parsons@mysociety.org>
2023-04-19 08:36:55 +0000

    HTTPs for WriteToThem link

--- www/docs/postcode/repexplain.php
+++ www/docs/postcode/repexplain.php
@@ -16,4 +16,4 @@
 
 <p><?= gettext("Your MP represents you in the House of Commons. The House of Commons is responsible for making laws in the UK and for overall scrutiny of all aspects of government.")?></p>
 
-<p><?= gettext("You can write to any representative, or your local councillors through <a href=\"http://www.writetothem.com/who?pc=" . $pc . "\">WriteToThem.com</a>")?>.</p>
+<p><?= gettext("You can write to any representative, or your local councillors through <a href=\"https://www.writetothem.com/who?pc=" . $pc . "\">WriteToThem.com</a>")?>.</p>
