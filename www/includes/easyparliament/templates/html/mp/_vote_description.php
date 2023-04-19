91ac582736e55d3d5d3dedd09973e20fb50e9060
Alex Parsons <alex.parsons@mysociety.org>
2023-04-19 08:33:23 +0000

    Make covid disclaimers harmless for non MPs
    - $covid_affected was only being set for MPs
    - Add isset check to fix MSPs, etc pages

--- www/includes/easyparliament/templates/html/mp/_vote_description.php
+++ www/includes/easyparliament/templates/html/mp/_vote_description.php
@@ -7,7 +7,7 @@
         <?= isset($key_vote) ? "$key_vote[summary]." : '' ?>
         <?= isset($party_voting_line) ? $party_voting_line : '' ?>
     </a>
-    <?php if ($covid_affected) { ?>
+    <?php if (isset($covid_affected) && $covid_affected) { ?>
     <span style="font-size:60%">Absences for this policy may be affected <a href="#covid-19">COVID-19 restrictions</a>.</span>
     <?php } ?>
     <?php } ?>
