ed6b36d078f14c566ce8019c0078dd87f08a4739
Alex Parsons <alex.parsons@mysociety.org>
2023-04-19 12:58:26 +0000

    Covid links from division list point to votes page
    - Consistency, only one place needs to stay up for these links to work.

--- www/includes/easyparliament/templates/html/mp/_division_description.php
+++ www/includes/easyparliament/templates/html/mp/_division_description.php
@@ -2,7 +2,7 @@
     <span class="policy-vote__date">On <?= strftime('%e %b %Y', strtotime($division['date'])) ?>:</span>
     <span class="policy-vote__text"><?= $full_name ?><?= $division['text'] ?></span>
     <?php if ($division['date'] > '2020-06-01' && $division['date'] < '2020-06-10' && $division['vote'] == 'absent') { ?>
-        <p class="vote-description__covid">This absence may have been affected by <a href="#covid-19">COVID-19 restrictions</a>.</p> 
+        <p class="vote-description__covid">This absence may have been affected by <a href="<?= $member_url ?>/votes#covid-19">COVID-19 restrictions</a>.</p> 
     <?php } ?>
     <a class="vote-description__source" href="/divisions/<?= $division['division_id'] ?>/mp/<?= $person_id ?>">Show vote</a>
 </li>
