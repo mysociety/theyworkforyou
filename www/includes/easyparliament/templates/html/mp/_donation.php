b0843b36b7c68b527fbb32ccb1830d9611cbf5ef
Alex Parsons <alex.parsons@mysociety.org>
2023-04-24 14:09:44 +0000

    Donation box in sidebar is customizable
    - Set as 'donation' location for annoucements.
    - Does not have an image.

--- www/includes/easyparliament/templates/html/mp/_donation.php
+++ www/includes/easyparliament/templates/html/mp/_donation.php
@@ -1,6 +1,15 @@
 
-<div class="sidebar__unit__donation">
-    <h3 class="content__title">Donate to TheyWorkForYou</h3>
-    <p class="content__description">We believe that information about our  representatives should be accessible to everyone.</p>
-    <a class="button content__button button--negative" href="https://www.mysociety.org/donate/?utm_source=theyworkforyou&utm_medium=website&utm_campaign=rep-sidebar">Can you support us?</a>
+
+<?php $featured_item = $annoucement_manager->get_random_valid_item("donation"); ?>
+
+<?php if ( $featured_item ) { ?>
+
+<div class="sidebar__unit__featured_side">
+  <div class="featured_side__content">
+  <a href="<?= $featured_item->url ?>"><h3 class="content__title"><?= $featured_item->title ?></h3></a>
+  <p class="content__description"><?= $featured_item->content ?></p>  
+  <a class="button content__button <?= $featured_item->button_class ?>" href="<?= $featured_item->url ?>"><?= (isset($featured_item->button_text) ? $featured_item->button_text : "Read more") ?></a>
+  </div>
 </div>
+
+<?php }; ?>
