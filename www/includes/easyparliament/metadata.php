38c180d21605ed816b647c18b0379af2385ec1b6
Alex Parsons <alex.parsons@mysociety.org>
2023-04-24 15:15:59 +0000

    Replaced Banner with AnnouncementManagement
    - Stores json config in 'banner' and 'announcements' editoral option.
    - Checks valid json (not valid schema) before save.

--- www/includes/easyparliament/metadata.php
+++ www/includes/easyparliament/metadata.php
@@ -174,9 +174,14 @@ $this->page = array (
         'url'			=> 'admin/policies.php',
     ),
     'admin_banner' => array (
-        'title'			=> 'Edit Banner',
+        'title'			=> 'Edit Banners',
         'parent'		=> 'admin',
-        'url'			=> 'admin/banner.php',
+        'url'			=> 'admin/banner.php?editorial_option=banner',
+    ),
+    'admin_announcement' => array (
+        'title'			=> 'Edit Announcements',
+        'parent'		=> 'admin',
+        'url'			=> 'admin/banner.php?editorial_option=announcements',
     ),
     'admin_featured' => array (
         'title'			=> 'Featured debates',
