bc9eff52bd8539c473b910550cfd220093eaaa7a
Alex Parsons <alex.parsons@mysociety.org>
2023-06-07 10:55:42 +0000

    Use GA4 event tracking
    - replaces measurement protocol approach

--- www/docs/js/main.js
+++ www/docs/js/main.js
@@ -9,46 +9,15 @@ var trackEvent = function(eventName, params) {
       dfd.resolve();
   };
 
-  // Returns either an array, or undefined.
-  var measurementProtocolDetails = window.dataLayer.filter(function(row){
-      return row[0] === 'measurement_protocol';
-  })[0];
+  // Tell Gtag to resolve our promise when it's done.
+  var params = $.extend(params, {
+      event_callback: callback
+  });
 
-  if (measurementProtocolDetails) {
-      var measurementParams = {
-          measurement_id: measurementProtocolDetails[1],
-          api_secret: measurementProtocolDetails[2]
-      }
-      if (measurementProtocolDetails[3]) {
-          params['debug_mode'] = '1';
-      }
+  gtag('event', eventName, params);
 
-      // Set a random client_id (2 32-bit integers seperated by a dot).
-      // Note this random approach means the GA debugView won't work.
-      // (To get that to work, you need to turn back on the cookies,
-      // then use the same client_id as in in the _ga cookie.)
-      var client_id = Math.floor(Math.random() * 1000000000) + '.' + Math.floor(Math.random() * 1000000000);
-
-      // print response to console
-      $.ajax({
-          url: 'https://www.google-analytics.com/mp/collect?' + $.param(measurementParams),
-          method: 'POST',
-          data: JSON.stringify({
-              client_id: client_id,
-              events: [{
-                  name: eventName,
-                  params: params
-              }]
-          })
-      }).always(callback);
-
-      // Wait a maximum of 2 seconds for ajax to resolve.
-      setTimeout(callback, 2000);
-
-  } else {
-      // Measurement Protocol not available. Resolve promise immediately.
-      callback();
-  }
+  // Wait a maximum of 2 seconds for Gtag to resolve promise.
+  setTimeout(callback, 2000);
 
   return dfd.promise();
 };
