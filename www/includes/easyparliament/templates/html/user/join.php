b4b8337ee82a64bef1ebbde4d01c5a3e5f0aee73
Alex Parsons <alex.parsons@mysociety.org>
2023-06-13 10:07:34 +0000

    Remove privacy info blurb on user join page.
    - Duplicates privacy information elsewhere.

--- www/includes/easyparliament/templates/html/user/join.php
+++ www/includes/easyparliament/templates/html/user/join.php
@@ -125,18 +125,7 @@
             </div> <!-- end .main -->
             <div class="sidebar">
               <div class="block" id="help">
-                <h4>Your privacy</h4>
-                <div class="blockbody">
 
-                <p>Welcome to <strong>TheyWorkForYou.com</strong> - the more you contribute and participate, the better the site will get for everyone.</p>
-
-                <p>Our <strong>Privacy Policy</strong> is very simple:</p>
-
-                <p><strong>1.</strong> We guarantee we will not sell or distribute any personal information you share with us</p>
-                <p><strong>2.</strong>We will not be sending you unsolicited email</p>
-                <p><strong>3.</strong>We will gladly show you the personal data we store about you in order to run the website</p>
-
-                <p>We hope you enjoy using the website.</p>
 
               </div>
             </div> 
