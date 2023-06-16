b007fb86ed5632fbee4d229dfbbb2b2fe5e5c7fc
Alex Parsons <alex.parsons@mysociety.org>
2023-06-16 08:28:21 +0000

    [NI] Updated NI description to reflect situation
    - Assembly is not suspended
    - There are MLAs
    - But the Assembly can't do anything

--- www/includes/easyparliament/templates/html/ni/index.php
+++ www/includes/easyparliament/templates/html/ni/index.php
@@ -93,15 +93,7 @@
                     <div class="homepage-upcoming homepage-content-section">
                         <h2>What is the Northern Ireland Assembly?</h2>
 
-                        <p>The current <strong>Northern Ireland Assembly</strong> was established in
-                        1998 as part of the Belfast Agreement. It was suspended on the 14th of October
-                        2002, and remained suspended until 8th May 2007.</p>
-
-                        <p>Between 8 May 2006 and 22 November 2006 the <strong>Assembly established
-                        under the Northern Ireland Act 2006</strong>, and between November 2006 and
-                        January 2007 the <strong>Transitional Assembly</strong> created by the Northern
-                        Ireland (St Andrews Agreement) Act 2006, met in order to prepare for the
-                        restoration of devolved government.</p>
+                        <?php include dirname(__FILE__) . '/../section/_ni_desc.php' ?>
                     </div>
                 </div>
             </div>
--- www/includes/easyparliament/templates/html/section/_ni_desc.php
+++ www/includes/easyparliament/templates/html/section/_ni_desc.php
@@ -1,12 +1,11 @@
                 <p>
-                The current Northern Ireland Assembly was established in 1998 as part of the
-                Belfast Agreement. It was suspended on the 14th of October 2002, and remained
-                suspended until 8th May 2007.
+                The current <b>Northern Ireland Assembly</b> was established in 1998 as part of the
+                Belfast Agreement.
                 </p>
 
                 <p>
-                Between 8 May 2006 and 22 November 2006 the Assembly established under the
-                Northern Ireland Act 2006, and between November 2006 and January 2007 the
-                Transitional Assembly created by the Northern Ireland (St Andrews Agreement)
-                Act 2006, met in order to prepare for the restoration of devolved government.
+                As of 2023, the assembly has sitting MLAs,
+                <a href="https://www.theyworkforyou.com/ni/?id=2023-02-14.1.2#g1.25"> has been unable to reach the cross-community agreement required to elect a speaker</a>.
+                
+                As such, there will be no debates in the Assembly until a speaker is elected.
                 </p>
