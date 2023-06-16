3c6a83cd312ffeb6d896e6fa6bd066b185f2cd79
Alex Parsons <alex.parsons@mysociety.org>
2023-06-16 10:12:59 +0100

    [ni] Fix typo in description

--- www/includes/easyparliament/templates/html/section/_ni_desc.php
+++ www/includes/easyparliament/templates/html/section/_ni_desc.php
@@ -4,8 +4,8 @@
                 </p>
 
                 <p>
-                As of 2023, the assembly has sitting MLAs,
-                <a href="https://www.theyworkforyou.com/ni/?id=2023-02-14.1.2#g1.25"> has been unable to reach the cross-community agreement required to elect a speaker</a>.
+                As of 2023, the Assembly has sitting MLAs,
+                <a href="https://www.theyworkforyou.com/ni/?id=2023-02-14.1.2#g1.25"> but has been unable to reach the cross-community agreement required to elect a speaker</a>.
                 
                 As such, there will be no debates in the Assembly until a speaker is elected.
                 </p>
