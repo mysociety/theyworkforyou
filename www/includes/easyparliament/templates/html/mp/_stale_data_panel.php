357d43e7901794113c96de4022ea64bbbb3304c0
Alex Parsons <alex.parsons@mysociety.org>
2023-08-29 16:18:16 +0000

    Re-add stale data warning.
    Add box warning flagging we're now out of date while we fix that.

---
+++ www/includes/easyparliament/templates/html/mp/_stale_data_panel.php
@@ -0,0 +1,8 @@
+<?php if (in_array(HOUSE_TYPE_COMMONS, $houses)): ?>
+<a name="stale-data-warning"></a>
+<div class="panel panel--highlight">
+
+    <p>There are currently votes on relevant topics that are not included in the summaries below. We are working on bringing these records up to date over the autumn. </p>
+
+</div>
+<?php endif; ?>
