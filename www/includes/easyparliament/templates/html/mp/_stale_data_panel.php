d8531ec53484d3da6d45bb94324762da211cc5f8
Alex Parsons <alex.parsons@mysociety.org>
2023-12-08 11:44:57 +0000

    Update stale data message

--- www/includes/easyparliament/templates/html/mp/_stale_data_panel.php
+++ www/includes/easyparliament/templates/html/mp/_stale_data_panel.php
@@ -2,7 +2,10 @@
 <a name="stale-data-warning"></a>
 <div class="panel panel--highlight">
 
-    <p>There are currently votes on relevant topics that are not included in the summaries below. We are working on bringing these records up to date over the autumn. </p>
+    <p>
+        There are currently votes on relevant topics that are not included in the summaries below.
+        We are working on a larger set of updates to these voting records that will be complete in early 2024.
+    </p>
 
 </div>
 <?php endif; ?>
