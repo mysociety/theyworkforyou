b63e82cc8102f1692cd21495ca9c3acffe4ad924
Alex Parsons <alex.parsons@mysociety.org>
2024-02-09 20:25:26 +0000

    Source voting information from markdown document

---
+++ classes/Renderer/Markdown.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MySociety\TheyWorkForYou\Renderer;
+
+/**
+ * Markdown Renderer
+ *
+ * Use for converting markdown into help pages.
+ */
+
+class Markdown
+{
+    public function markdown_document($this_page){
+        // This function takes a markdown file and converts it to HTML
+    
+        $markdown_file = '../../../markdown/' . $this_page . '.md';
+        $Parsedown = new \Parsedown();
+        
+        $text = file_get_contents($markdown_file);
+        $html = $text;
+        $html = $Parsedown->text($text);
+        
+        # title is the first h1
+        preg_match('/<h1>([^<]+)<\/h1>/i', $html, $matches);
+        
+        $title = $matches[1];
+        
+        $html = preg_replace_callback('/<h([1-3])>([^<]+)<\/h[1-3]>/i', function($matches) {
+            $level = $matches[1];
+            $htitle = $matches[2];
+            $slug = slugify($htitle);
+            return "<h$level id=\"$slug\" class=\"js-toc-item\">$htitle</h$level>";
+        }, $html);
+        
+        
+        \MySociety\TheyWorkForYou\Renderer::output('static/markdown_template', array(
+            'html' => $html,
+            'this_page' => $this_page,
+            'page_title' => $title,
+        ));
+        
+    }
+}
---
+++ www/includes/easyparliament/templates/html/static/markdown_template.php
@@ -0,0 +1,14 @@
+<div class="full-page static-page toc-page">
+    <div class="full-page__row">
+        <div class="toc-page__col">
+<div class="toc js-toc"></div>
+        </div>
+        <div class="toc-page__col">
+
+            <div class="panel">
+                <?= $html ?>
+            </div>
+        </div>  
+
+    </div>
+</div>
