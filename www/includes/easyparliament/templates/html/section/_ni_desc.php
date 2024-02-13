d8326a9e32dd76edbe6edb6b6d81f207041e1c4d
Alex Parsons <alex.parsons@mysociety.org>
2024-02-13 20:17:41 +0000

    Remove NI Assembly no speaker text

--- www/includes/easyparliament/templates/html/section/_ni_desc.php
+++ www/includes/easyparliament/templates/html/section/_ni_desc.php
@@ -2,10 +2,3 @@
                 The current <b>Northern Ireland Assembly</b> was established in 1998 as part of the
                 Belfast Agreement.
                 </p>
-
-                <p>
-                As of 2023, the Assembly has sitting MLAs,
-                <a href="https://www.theyworkforyou.com/ni/?id=2023-02-14.1.2#g1.25"> but has been unable to reach the cross-community agreement required to elect a speaker</a>.
-                
-                As such, there will be no debates in the Assembly until a speaker is elected.
-                </p>
