c3c0ac4d6ccddf51c623122b8878c39339adf292
Alex Parsons <alex.parsons@mysociety.org>
2024-03-04 17:19:33 +0000

    Add MP info page to top of profile

---
+++ www/includes/easyparliament/templates/html/mp/_chamber_info_panel.php
@@ -0,0 +1,23 @@
+<?php if ($this_page == "mp"): ?>
+<div class="panel">
+    <h2>About your Member of Parliament</h2>
+    <p>
+    Your MP (<?= ucfirst($full_name) ?>) represents you, and all of the people who live in <?= $latest_membership['constituency'] ?>,
+        at the UK Parliament in Westminster.
+    </p>
+    <p>
+    MPs split their time between Parliament and their constituency.
+        In Parliament, they debate and vote on new laws, review existing laws, and question the Government.
+        In the constituency, their focus is on supporting local people and championing local issues.
+        They have a small staff team who help with casework, maintain their diaries, and monitor their inbox.
+    </p>
+    <h2>
+    What you can do
+    </h2>
+    <ul class="rep-actions" style="li: > list-style: default">
+        <li>Find out <a href="#profile">more about your MP</a>, including <a href="<?= $member_url ?>/votes">their voting summary</a> and <a href="#appearances">recent speeches</a>.</li>
+        <li><a href="https://www.writetothem.com/">Write to your MP</a>, or find out about your other local representatives <a href="https://www.writetothem.com">on WriteToThem.com</a>.</li>
+        <li>Find out more about <a href="https://www.localintelligencehub.com/area/WMC/<?= $latest_membership['constituency'] ?>"><?= $latest_membership['constituency'] ?></a> on the <a href="https://www.localintelligencehub.com/">Local Intelligence Hub</a>.</li>
+    </ul>
+</div>
+<?php endif; ?>
