4cc5ceb27ff686d5c2d4800cad263e80f0c0ce9d
Alex Parsons <alex.parsons@mysociety.org>
2024-06-05 13:32:58 +0000

    Common footer across MP subpages

--- www/includes/easyparliament/templates/html/mp/profile.php
+++ www/includes/easyparliament/templates/html/mp/profile.php
@@ -248,35 +248,7 @@ $display_wtt_stats_banner = '2015';
                 </div>
                 <?php endif; ?>
 
-                <div class="panel panel--secondary">
-                    <p><?= gettext('Note for journalists and researchers: The data on this page may be used freely, on condition that TheyWorkForYou.com is cited as the source.') ?></p>
-
-                    <p><?php print gettext('This data was produced by TheyWorkForYou from a variety of sources.') . ' ';
-                        printf(gettext('Voting information from <a href="%s">Public Whip</a>.'), "https://www.publicwhip.org.uk/mp.php?id=uk.org.publicwhip/member/ $member_id&amp;showall=yes"); ?></p>
-
-                  <?php if($image && $image['exists']) {
-                      if(isset($data['photo_attribution_text'])) { ?>
-                    <p>
-                      <?php if(isset($data['photo_attribution_link'])) { ?>
-                        <?= gettext('Profile photo:') ?>
-                        <a href="<?= $data['photo_attribution_link'] ?>"><?= $data['photo_attribution_text'] ?></a>
-                      <?php } else { ?>
-                        <?= gettext('Profile photo:') ?> <?= $data['photo_attribution_text'] ?>
-                      <?php } ?>
-                    </p>
-                  <?php }
-                  } else { ?>
-                    <p>
-                        We&rsquo;re missing a photo of <?= $full_name ?>.
-                        If you have a photo <em>that you can release under
-                        a Creative Commons Attribution-ShareAlike license</em>
-                        or can locate a <em>copyright free</em> photo,
-                        <a href="mailto:<?= str_replace('@', '&#64;', CONTACTEMAIL) ?>">please email it to us</a>.
-                        Please do not email us about copyrighted photos
-                        elsewhere on the internet; we can&rsquo;t use them.
-                    </p>
-                  <?php } ?>
-                </div>
+                <?php include('_profile_footer.php'); ?>
 
             </div>
         </div>
