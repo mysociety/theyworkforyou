d520da7a706108d0f8dd71f6a50a1dafc96be1af
Alex Parsons <alex.parsons@mysociety.org>
2024-07-05 12:23:20 +0000

    Update MP donate message

--- www/includes/easyparliament/templates/html/mp/_chamber_info_panel.php
+++ www/includes/easyparliament/templates/html/mp/_chamber_info_panel.php
@@ -45,8 +45,17 @@
 <?php } ?>
 
 <div class="panel">
-    <hr >
+<h2>Support TheyWorkForYou's work in the new Parliament</h2>
+
+    <p>TheyWorkForYou is run by <a href="https://www.mysociety.org/">mySociety</a>, a small UK charity.</p>
+    
+    <p>We're a very efficient operation and do a lot with a small team. At the moment TheyWorkForYou, which is used by millions of people each year, is run with less than the equivalent of one full-time person.</p>
+
+    <p>If we had <a href="/support-us/">a <strong>bit</strong> more money, we could achieve a <strong>lot</strong> more</a>.</p>
     <p>We want MPs to meet the standards and expectations of the people who elected them - <strong>you</strong>!</p>
+
+    <p>If you share that goal <a href="/support-us/">please donate today</a> to enable greater transparency and accountability of the next government.</p>
+    
     <p>Learn more about <a href="/support-us/?utm_source=theyworkforyou.com&utm_content=postcode+donate&utm_medium=link&utm_campaign=postcode&how-much=5">our current work</a>, and <a href="https://www.mysociety.org/democracy/who-funds-them/">our new project WhoFundsThem</a> - looking into MPs’ and APPGs’ financial interests.</p>
                     <div class="inline-donation-box">
                         
