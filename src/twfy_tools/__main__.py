42b8e59d445fe667786b0942b54465bfbb8c7d3b
Alex Parsons <alex.parsons@mysociety.org>
2024-08-18 19:14:21 +0000

    Migrate division_io to new python setup
    - hooked into generic connection and config
    - hooked into overall cli

--- src/twfy_tools/__main__.py
+++ src/twfy_tools/__main__.py
@@ -1,6 +1,9 @@
-from .common.typer_helper import MysocTyper
+from .common.typer_helper import MysocTyper, TyperModule
+from .utils import division_io
 
 app = MysocTyper()
+
+app = app.add_submodules(typer_groups=[TyperModule(division_io.app, "divisions-io")])
 
 if __name__ == "__main__":
     app()
