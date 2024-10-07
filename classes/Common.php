33b7249746ba769654438f3df25248ac3f1ebfff
Alex Parsons <alex.parsons@mysociety.org>
2024-10-07 12:54:27 +0000

    Reduce recent searches to 3

--- classes/Common.php
+++ classes/Common.php
@@ -5,7 +5,7 @@ namespace MySociety\TheyWorkForYou;
 class Common {
     public function getPopularSearches() {
         global $SEARCHLOG;
-        $popular_searches = $SEARCHLOG->popular_recent(5, 2000, 5);
+        $popular_searches = $SEARCHLOG->popular_recent(3, 2000, 5);
 
         return $popular_searches;
     }
