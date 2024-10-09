bb9a867b203b0222bb8d9ca38b4584e8c8ace05f
Struan Donald <struan@exo.org.uk>
2024-10-09 16:14:59 +0100

    optionally return alert parts from prettify and add parts to alerts
    To make it a bit easier to pick out the different parts of an alert
    update prettifyCritera to return the parts as a hash instead of turning
    it into a string.

--- classes/Utility/Alert.php
+++ classes/Utility/Alert.php
@@ -34,6 +34,7 @@ class Alert {
         $alerts = [];
         foreach ($q as $row) {
             $criteria = self::prettifyCriteria($row['criteria']);
+            $parts = self::prettifyCriteria($row['criteria'], true);
             $token = $row['alert_id'] . '-' . $row['registrationtoken'];
 
             $status = 'confirmed';
@@ -43,21 +44,28 @@ class Alert {
                 $status = 'suspended';
             }
 
-            $alerts[] = [
+            $alert = [
                 'token' => $token,
                 'status' => $status,
                 'criteria' => $criteria,
                 'raw' => $row['criteria'],
+                'keywords' => [],
+                'exclusions' => [],
             ];
+
+            $alert = array_merge($alert, $parts);
+
+            $alerts[] = $alert;
         }
 
         return $alerts;
     }
 
-    public static function prettifyCriteria($alert_criteria) {
+    public static function prettifyCriteria($alert_criteria, $as_parts = false) {
         $text = '';
         if ($alert_criteria) {
             $criteria = explode(' ', $alert_criteria);
+            $parts = [];
             $words = [];
             $spokenby = array_values(\MySociety\TheyWorkForYou\Utility\Search::speakerNamesForIDs($alert_criteria));
 
@@ -68,11 +76,18 @@ class Alert {
             }
             if ($spokenby && count($words)) {
                 $text = implode(' or ', $spokenby) . ' mentions [' . implode(' ', $words) . ']';
+                $parts['spokenby'] = $spokenby;
+                $parts['words'] = $words;
             } elseif (count($words)) {
                 $text = '[' . implode(' ', $words) . ']' . ' is mentioned';
+                $parts['words'] = $words;
             } elseif ($spokenby) {
                 $text = implode(' or ', $spokenby) . " speaks";
+                $parts['spokenby'] = $spokenby;
             }
+        }
+        if ($as_parts) {
+            return $parts;
         }
         return $text;
     }
