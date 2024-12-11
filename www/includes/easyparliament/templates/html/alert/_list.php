240cb229935487ab9929fa70fc6e3fb6d2fabb7a
Lucas Cumsille M <hello@lucascm.dev>
2024-12-11 10:22:12 +0000

    move alerts details from user page

--- www/includes/easyparliament/templates/html/alert/_list.php
+++ www/includes/easyparliament/templates/html/alert/_list.php
@@ -1,27 +1,15 @@
-<h3><?= gettext('Your current email alerts') ?></h3>
+<hr>
+<h3><?= gettext('Email alerts') ?></h3>
 
-<ul class="alerts-manage__list">
-  <?php foreach ($alerts as $alert) { ?>
-    <li>
-        <?= sprintf(gettext('When %s'), _htmlspecialchars($alert['criteria'])) ?>.
-        <form action="<?= $actionurl ?>" method="POST">
-            <input type="hidden" name="t" value="<?= _htmlspecialchars($alert['token']) ?>">
-          <?php if ($alert['status'] == 'unconfirmed') { ?>
-            <input type="submit" class="button small" name="action" value="<?= gettext('Confirm') ?>">
-          <?php } elseif ($alert['status'] == 'suspended') { ?>
-            <input type="submit" class="button small" name="action" value="<?= gettext('Resume') ?>">
-          <?php } else { ?>
-            <input type="submit" class="button button--secondary small" name="action" value="<?= gettext('Suspend') ?>">
-            <input type="submit" class="button button--negative small" name="action" value="<?= gettext('Delete') ?>">
-          <?php } ?>
-        </form>
-    </li>
-  <?php } ?>
-</ul>
+<?php $total_alerts = isset($alerts) ? count($alerts) : 0; ?>
+<p><?= sprintf(gettext('You currently have %d email alerts'), $total_alerts) ?></p>
+
+<a href="/alert/" class="button radius">Check here to see your alerts</a>
+
 
 <div class="clearfix">
     <form action="<?= $actionurl ?>" method="POST" class="pull-right">
-        <input type="hidden" name="t" value="<?= _htmlspecialchars($alert['token']) ?>">
+        <!-- No need to reference $alert['token'] here, as you're deleting all alerts -->
         <input type="submit" class="button button--negative small" name="action" value="<?= gettext('Delete All') ?>">
     </form>
 </div>
