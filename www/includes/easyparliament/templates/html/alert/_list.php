508d2ae61cd3a901c8678c1451b48aef061b7f11
Lucas Cumsille M <hello@lucascm.dev>
2024-12-16 11:17:28 +0000

    Improved copy for "delete all" button for alerts
    Without context it was a bit unclear what the "Delete all" button does. The new copy clarifies the button will delete all alerts. The new aria-label will also make sure assistive devices know this includes keywords and representative alerts.

--- www/includes/easyparliament/templates/html/alert/_list.php
+++ www/includes/easyparliament/templates/html/alert/_list.php
@@ -10,6 +10,6 @@
 <div class="clearfix">
     <form action="<?= $actionurl ?>" method="POST" class="pull-right">
         <!-- No need to reference $alert['token'] here, as you're deleting all alerts -->
-        <input type="submit" class="button button--negative small js-confirm-delete" name="action" value="<?= gettext('Delete All') ?>">
+        <input type="submit" class="button button--negative small js-confirm-delete" name="action" value="<?= gettext('Delete all alerts') ?>" aria-label="Delete all keywords and representatives alerts">
     </form>
 </div>
