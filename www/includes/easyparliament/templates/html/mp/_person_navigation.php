04a757b44ba2d9ac40ba5a52b280506b21b6e99b
Alex Parsons <alex.parsons@mysociety.org>
2025-02-17 13:03:39 +0000

    Updated register display
    This moves the display logic into twfy proper.
    Rather than in parlparse or the importer.
    
    This adds a menu structure in the side bar by cat.
    This also enables multiple registers for people in
    multiple chambers.
    
    It needs to account for a few different scenarios.
    - Commons/Wales - where there are more details.
    - Scotland/NI - where there is only free text content.
    - Historical content - raw XML.

--- www/includes/easyparliament/templates/html/mp/_person_navigation.php
+++ www/includes/easyparliament/templates/html/mp/_person_navigation.php
@@ -1,14 +1,15 @@
 <div class="person-navigation">
+
     <ul>
         <li <?php if ($pagetype == ""): ?>class="active"<?php endif; ?>><a href="<?= $member_url ?>"><?= gettext('Overview') ?></a></li>
           <?php if ($this_page == "mp"): ?>
             <li <?php if ($pagetype == "votes"): ?>class="active"<?php endif; ?>><a href="<?= $member_url ?>/votes"><?= gettext('Voting Summary') ?></a></li>
           <?php endif; ?>
           <?php if (in_array($this_page, ["mp", "msp", "ms"])): ?>
-          <li <?php if ($pagetype == "recent"): ?>class="active"<?php endif; ?>><a href="<?= $member_url ?>/recent"><?= gettext('Recent Votes') ?></a></li>
-            <?php if ($register_interests): ?>
-                <li <?php if ($pagetype == "register"): ?>class="active"<?php endif; ?>><a href="<?= $member_url ?>/register"><?= gettext('Register of Interests') ?></a></li>
-            <?php endif; ?>
+            <li <?php if ($pagetype == "recent"): ?>class="active"<?php endif; ?>><a href="<?= $member_url ?>/recent"><?= gettext('Recent Votes') ?></a></li>
           <?php endif; ?>
-          </ul>
+          <?php if ($register_interests): ?>
+            <li <?php if ($pagetype == "register"): ?>class="active"<?php endif; ?>><a href="<?= $member_url ?>/register"><?= gettext('Register of Interests') ?></a></li>
+          <?php endif; ?>
+    </ul>
 </div>
