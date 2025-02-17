c21db4574007abc8f425ad1b4d667b8a6de21989
Alex Parsons <alex.parsons@mysociety.org>
2025-02-17 12:58:54 +0000

    Add python based personinfo loader
    - Create new scripts/personinfo to load personinfo
    - Specifically loads the new regmem format and
    - makes sure data loaded is validated against it.
    - Adds pydantic and mysoc-validator to the project

---
+++ src/twfy_tools/utils/personinfo.py
@@ -0,0 +1,110 @@
+#!.venv/bin/python
+
+from typing import Literal, Optional
+
+import rich
+from mysoc_validator.models.interests import RegmemPerson, RegmemRegister
+from tqdm import tqdm
+from typer import Typer
+from typing_extensions import TypeGuard
+
+from twfy_tools.common.config import config
+from twfy_tools.db.utils import upload_person_info
+
+app = Typer(pretty_exceptions_enable=False)
+
+
+def is_valid_language(lang: str) -> TypeGuard[Literal["en", "cy"]]:
+    """
+    Typeguard to convert confirm string in Literal["en", "cy"]
+    """
+    return lang in {"en", "cy"}
+
+
+def prepare_person_regmem(
+    chamber: str = "commons",
+    *,
+    language: Literal["en", "cy"] = "en",
+    quiet: bool = False,
+):
+    """
+    Extract the most recent register of interest for a person in this chamber and send for upload
+    to the database.
+    """
+    if not quiet:
+        rich.print(f"Processing {chamber} register of members' interests")
+
+    source_path = config.RAWDATA / "scrapedjson" / "common_regmem" / chamber
+
+    if chamber == "senedd":
+        source_path = source_path / language
+
+    id_to_person: dict[int, RegmemPerson] = {}
+
+    for file in tqdm(
+        list(source_path.glob("*.json")), disable=quiet, desc="Processing sources"
+    ):
+        register = RegmemRegister.from_path(file)
+        for person in register.persons:
+            int_id = int(person.person_id.split("/")[-1])
+            if int_id not in id_to_person:
+                id_to_person[int_id] = person
+            else:
+                # check if we have a more modern version
+                existing = id_to_person[int_id]
+                if person.published_date > existing.published_date:
+                    id_to_person[int_id] = person
+
+    if not quiet:
+        rich.print(f"Found [blue]{len(id_to_person)}[/blue] person items to upload")
+
+    upload_person_info(
+        f"person_regmem_{chamber}_{language}",
+        id_to_person,
+        remove_absent=True,
+        quiet=quiet,
+    )
+
+
+@app.command()
+def upload_all_regmem(quiet: bool = False):
+    """
+    Upload regmems for all known chambers
+    """
+
+    # english chambers
+    chambers = ["commons", "scottish-parliament", "northern-ireland-assembly", "senedd"]
+    for chamber in chambers:
+        prepare_person_regmem(chamber, quiet=quiet, language="en")
+
+    # welsh chambers
+    prepare_person_regmem("senedd", quiet=quiet, language="cy")
+
+
+@app.command()
+def upload_regmem(
+    quiet: bool = False,
+    all: bool = False,
+    chamber: Optional[str] = None,
+    language: str = "en",
+):
+    """
+    Upload register of members' interests to the database for one chamber
+    """
+
+    if all:
+        upload_all_regmem(quiet=quiet)
+    else:
+        if chamber is None:
+            raise ValueError("You must specify a chamber if not uploading all")
+        if is_valid_language(language):
+            prepare_person_regmem(chamber, quiet=quiet, language=language)
+
+
+@app.callback()
+def callback():
+    pass
+
+
+if __name__ == "__main__":
+    app()
