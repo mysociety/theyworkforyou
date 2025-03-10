6d3e2ee5fd6d2afd6d79bd9ea12e378b22a1695c
Alex Parsons <alex.parsons@mysociety.org>
2025-03-10 21:48:43 +0000

    Fetch regmem data from json directly

--- classes/DataClass/Regmem/Register.php
+++ classes/DataClass/Regmem/Register.php
@@ -25,6 +25,50 @@ class Register extends BaseModel {
         return null;
     }
 
+    public static function getDate(string $chamber, string $date): Register {
+        $file_dir = RAWDATA . "scrapedjson/universal_format_regmem/" . $chamber . "/";
+        $file_end = $date . ".json";
+        // see if there's a file that matches this - but might have a bit in the middle
+        $files = glob($file_dir . "*" . $file_end);
+        if (count($files) === 0) {
+            throw new \Exception("No register found for $chamber on $date");
+        }
+        if (count($files) > 1) {
+            throw new \Exception("Multiple registers found for $chamber on $date");
+        }
+        return self::fromFile($files[0]);
+    }
+
+    public static function latestAsOfDate(string $chamber, string $date): Register {
+        $file_dir = RAWDATA . "scrapedjson/universal_format_regmem/" . $chamber . "/";
+        $files = glob($file_dir . "*.json");
+        if (count($files) === 0) {
+            throw new \Exception("No register found for $chamber");
+        }
+        # sort most recent to least, find the first one before the date
+        rsort($files);
+        foreach ($files as $file) {
+            $file_date = basename($file, ".json");
+            if ($file_date <= $date) {
+                return self::fromFile($file);
+            }
+        }
+        throw new \Exception("No register found for $chamber on or before $date");
+    }
+
+    public static function getLatest(string $chamber): Register {
+        $file_dir = RAWDATA . "scrapedjson/universal_format_regmem/" . $chamber . "/";
+        if ($chamber == "senedd") {
+            $file_dir .= LANGUAGE . "/";
+        }
+        $files = glob($file_dir . "*.json");
+        if (count($files) === 0) {
+            throw new \Exception("No register found for $chamber");
+        }
+        $latest = max($files);
+        // raise exception with name of $latest
+        return self::fromFile($latest);
+    }
 
     public static function getMisc(string $file): Register {
         $file_path = RAWDATA . "scrapedjson/universal_format_regmem/misc/" . $file;
