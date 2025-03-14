5b2e3793e37796460539005aa44bbd06abeb4e7e
Alex Parsons <alex.parsons@mysociety.org>
2025-03-14 16:51:30 +0000

    Add a 'just new' button in uk/scot register

--- www/includes/easyparliament/templates/html/interests/category.php
+++ www/includes/easyparliament/templates/html/interests/category.php
@@ -46,16 +46,24 @@
                         continue;
                     }; ?>
                     <h2 id="category-<?= $category_id ?>"><?= $category_emojis[$selected_category_id] ?><?= $category_name ?></h2>
-                    <button id="toggleButton" onclick="toggleDetails()" style="display:none">Expand All</button>
-
+                    <div style="display: flex; gap: 1rem;">
+                    <button id="detailsToggleButton" onclick="toggleDetails()" style="display:none">Expand All</button>
+                    <button id="newToggleButton" onclick="toggleNewDetails()"; style="display:none">Just New</button>
+                </div>
                     <?php foreach ($register->persons as $person) { ?>
                         <?php foreach ($person->categories as $person_category) { ?>
                         <?php if ($person_category->category_id != $selected_category_id || $person_category->only_null_entries()) {
                             continue;
                         }; ?> 
+                        <div class="person-items <?php if ($person_category->only_old_entries($register->published_date)) { ?>old_entry<?php } ?>">
                         <h3><a href="/mp/<?= $person->intId() ?>/register"><?= $person->person_name ?></a></h3>
                         <?php foreach ($person_category->entries as $entry) { ?>
+                                <div class="interest-item <?= $entry->isNew($register->published_date) ? 'new_entry' : 'old_entry'; ?>" id="<?= $entry->comparable_id ?>">
+                                <?php if ($entry->isNew($register->published_date)) { ?>
+                                    <p>🆕<strong><?= gettext('New entry or subentry') ?></strong></p>
+                                <?php }; ?>
                                 <p><?= $entry->content ?>
+                                <p> 
                                 <?php if ($entry->hasEntryOrDetail()) { ?>
                                 <details>
                                 <summary>More details</summary>
@@ -63,8 +71,10 @@
                                 <?php include('_register_entry.php'); ?>
                                 </details></p>
                                 <?php }; ?>
+                                </div>
                         <?php }; ?>
-                                <hr/>
+                        <hr/>
+                        </div>
                         <?php }; ?>
                     <?php }; ?>
                 <?php }; ?>
@@ -78,8 +88,30 @@
 <script>
 
 if (document.querySelectorAll('details').length > 0) {
-    document.getElementById('toggleButton').style.display = 'block';
+    document.getElementById('detailsToggleButton').style.display = 'block';
 }
+
+if (document.querySelectorAll('.new_entry').length > 0) {
+    document.getElementById('newToggleButton').style.display = 'block';
+}
+
+new_entries_only = false;
+
+function toggleNewDetails() {
+    // this 
+    const old_entries = document.querySelectorAll('.old_entry');
+
+    new_entries_only = !new_entries_only;
+    if (new_entries_only) {
+        old_entries.forEach(d => d.style.display = 'none');
+        document.getElementById('newToggleButton').textContent = 'Show all';
+    } else {
+        old_entries.forEach(d => d.style.display = 'block');
+        document.getElementById('newToggleButton').textContent = 'Just new';
+    }
+
+}
+
 
 function toggleDetails() {
   const details = document.querySelectorAll('details');
@@ -95,11 +127,11 @@ function toggleDetails() {
   if (allOpen) {
     // If all are open, close them
     details.forEach(d => d.open = false);
-    document.getElementById('toggleButton').textContent = 'Expand All';
+    document.getElementById('detailsToggleButton').textContent = 'Expand All';
   } else {
     // If at least one is closed, open them all
     details.forEach(d => d.open = true);
-    document.getElementById('toggleButton').textContent = 'Collapse All';
+    document.getElementById('detailsToggleButton').textContent = 'Collapse All';
   }
 }
 </script>
