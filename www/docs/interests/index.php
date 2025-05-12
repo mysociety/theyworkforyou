ea9112ddf28496dd34cdead70d444273e189f432
Alex Parsons <alex.parsons@mysociety.org>
2025-05-12 11:59:13 +0000

    Specify page title for register of interests page
    - Problem with emoji

--- www/docs/interests/index.php
+++ www/docs/interests/index.php
@@ -7,4 +7,6 @@ include_once '../../includes/easyparliament/init.php';
 use MySociety\TheyWorkForYou\Renderer\Markdown;
 
 $markdown = new Markdown();
-$markdown->markdown_document('interests_home');
+$markdown->markdown_document('interests_home', true, [
+    '_page_title' => 'Registers of Interest',
+    '_social_image_title' => 'Registers of Interest']);
