8c9423a123898dfcb7bdd2a6021a8b08548a0194
Alex Parsons <alex.parsons@mysociety.org>
2025-05-12 08:06:54 +0000

    Add social image for alerts page

--- www/docs/alert/index.php
+++ www/docs/alert/index.php
@@ -7,5 +7,7 @@ include_once INCLUDESPATH . 'easyparliament/member.php';
 
 $alert = new MySociety\TheyWorkForYou\AlertView\Standard($THEUSER);
 $data = $alert->display();
-
+$data["og_image"] = MySociety\TheyWorkForYou\Url::generateSocialImageUrl(
+    gettext("Email Alerts"),
+);
 MySociety\TheyWorkForYou\Renderer::output('alert/index', $data);
