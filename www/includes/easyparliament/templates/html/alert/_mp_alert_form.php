bf87eacc160a5fa36bbe9581e35ee66aacb32ffa
Struan Donald <struan@exo.org.uk>
2025-08-07 14:31:06 +0100

    split alert form abandon button to include
    and make sure it distinguished between editing a creating an alert

--- www/includes/easyparliament/templates/html/alert/_mp_alert_form.php
+++ www/includes/easyparliament/templates/html/alert/_mp_alert_form.php
@@ -89,7 +89,11 @@
           <?php } ?>
           <button type="submit" class="button button--red" name="action" value="Abandon">
             <i aria-hidden="true" class="fi-trash"></i>
-            <span><?= gettext('Abandon changes') ?></span>
+            <?php if ($token) { ?>
+              <span><?= gettext('Abandon changes') ?></span>
+            <?php } else { ?>
+              <span><?= gettext('Abandon create') ?></span>
+            <?php } ?>
           </button>
       </p>
 
