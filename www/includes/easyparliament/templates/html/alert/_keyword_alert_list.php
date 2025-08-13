0ace28d16b27bc1d65b0c19c4e21958e69b8536c
Struan Donald <struan@exo.org.uk>
2025-08-13 17:26:13 +0100

    move alert controls into include
    Suspend/Delete etc buttons

--- www/includes/easyparliament/templates/html/alert/_keyword_alert_list.php
+++ www/includes/easyparliament/templates/html/alert/_keyword_alert_list.php
@@ -12,40 +12,7 @@
                       </button>
                       <div id="accordion-content-<?= $index ?>" class="keyword-alert-accordion__content js-accordion-content" aria-hidden="true" role="img">
                         <div class="keyword-alert-accordion__content-header">
-                          <div class="alert__controls">
-                            <form action="<?= $actionurl ?>" method="POST">
-                              <input type="hidden" name="t" value="<?= _htmlspecialchars($alert['token']) ?>">
-                              <?php if ($alert['status'] == 'unconfirmed') { ?>
-                                <button type="submit" class="button small" name="action" value="Confirm">
-                                  <i aria-hidden="true" class="fi-save"></i>
-                                  <span><?= gettext('Confirm alert') ?></span>
-                                </button>
-                              <?php } elseif ($alert['status'] == 'suspended') { ?>
-                                <button type="submit" class="button small" name="action" value="Resume">
-                                  <span><?= gettext('Resume alert') ?></span>
-                                  <i aria-hidden="true" class="fi-play"></i>
-                                </button>
-                              <?php } else { ?>
-                                <button type="submit" class="button button--outline small" name="action" value="Suspend">
-                                  <i aria-hidden="true" class="fi-pause"></i>
-                                  <span><?= gettext('Suspend alert') ?></span>
-                                </button>
-                                <button type="submit" class="button small button--outline-red" name="action" value="Delete">
-                                  <i aria-hidden="true" class="fi-trash"></i>
-                                  <span><?= gettext('Delete alert') ?></span>
-                                </button>
-                              </form>
-                              <form action="<?= $actionurl ?>" method="POST">
-                                <input type="hidden" name="step" value="define">
-                                <input type="hidden" name="shown_related" value="1">
-                                <input type="hidden" name="t" value="<?= _htmlspecialchars($alert['token']) ?>">
-                                <button type="submit" class="button button--outline small" value="Edit">
-                                  <i aria-hidden="true" class="fi-page-edit"></i>
-                                  <span><?= gettext('Edit alert') ?></span>
-                                </button>
-                              <?php } ?>
-                            </form>
-                          </div>
+                          <?php include "_alert_controls.php" ?>
                           <dl class="alert-meta-info">
                             <?php if (array_key_exists("search_results", $alert)) { ?>
                               <div class="content-header-item">
@@ -174,57 +141,7 @@
                     <h3><?= _htmlspecialchars(implode(', ', $person_alerts[0]['spokenby'])) ?></h3>
                     <?php foreach ($person_alerts as $alert) { ?>
                       <p class="alert-form__subtitle"><?= _htmlspecialchars($alert['criteria']) ?></p>
-                      <div class="alert__controls">
-                        <form action="<?= $actionurl ?>" method="POST">
-                          <input type="hidden" name="t" value="<?= _htmlspecialchars($alert['token']) ?>">
-                          <?php if ($alert['status'] == 'unconfirmed') { ?>
-                            <button type="submit" class="button small" name="action" value="Confirm">
-                              <i aria-hidden="true" class="fi-save"></i>
-                              <span><?= gettext('Confirm alert') ?></span>
-                            </button>
-                          <?php } elseif ($alert['status'] == 'suspended') { ?>
-                            <button type="submit" class="button button-outline small" name="action" value="Resume">
-                              <span><?= gettext('Resume alert') ?></span>
-                              <i aria-hidden="true" class="fi-play"></i>
-                            </button>
-                          <?php } else { ?>
-                            <button type="submit" class="button button--outline small" name="action" value="Suspend">
-                              <i aria-hidden="true" class="fi-pause"></i>
-                              <span><?= gettext('Suspend alert') ?></span>
-                            </button>
-                            <button type="submit" class="button small button--outline-red" name="action" value="Delete">
-                              <i aria-hidden="true" class="fi-trash"></i>
-                              <span><?= gettext('Delete alert') ?></span>
-                            </button>
-                          <?php } ?>
-                        </form>
-                        <form action="<?= $actionurl ?>" method="POST">
-                          <?php if (count($alert['words'])) { ?>
-                            <input type="hidden" name="step" value="define">
-                            <input type="hidden" name="shown_related" value="1">
-                            <button type="submit" class="button button--outline small" value="Edit">
-                              <i aria-hidden="true" class="fi-page-edit"></i>
-                              <span><?= gettext('Edit alert') ?></span>
-                            </button>
-                          <?php } else { ?>
-                            <input type="hidden" name="mp_step" value="mp_confirm">
-                            <input type="hidden" name="pid" value="<?= $alert['pid'] ?>">
-                            <?php if ($alert['ignore_speaker_votes'] == 1) { ?>
-                              <input type="hidden" name="ignore_speaker_votes" value="0">
-                              <button type="submit" class="button button--outline small" value="Edit">
-                                <i aria-hidden="true" class="fi-page-edit"></i>
-                                <span><?= gettext('Include votes') ?></span>
-                              </button>
-                            <?php } else { ?>
-                              <input type="hidden" name="ignore_speaker_votes" value="1">
-                              <button type="submit" class="button button--outline small" value="Edit">
-                                <i aria-hidden="true" class="fi-page-edit"></i>
-                                <span><?= gettext('Ignore votes') ?></span>
-                              </button>
-                            <?php } ?>
-                          <?php } ?>
-                        </form>
-                      </div>
+                      <?php include "_alert_controls.php" ?>
                     <?php } ?>
                   </div>
                 <?php } ?>
