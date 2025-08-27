a30c11879eec27d50615b0bb1606313a0d238878
Alex Parsons <alex.parsons@mysociety.org>
2025-08-27 13:19:22 +0000

    Remove GA universal tracking code
    - Had both for the overlap period - but not needed now

--- www/includes/easyparliament/templates/html/header.php
+++ www/includes/easyparliament/templates/html/header.php
@@ -74,16 +74,6 @@
         gtag('config','<?= GOOGLE_ANALYTICS_ID ?>', {'client_id': client_id, 'cookie_expires': 1 });
     </script>
 
-    <script>
-        (function (i,s,o,g,r,a,m) {i['GoogleAnalyticsObject']=r;i[r]=i[r]||function () {
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-660910-1', {'storage':'none'});
-        ga('set', 'anonymizeIp', true);
-        ga('send', 'pageview');
-    </script>
   <?php endif; ?>
 
 </head>
