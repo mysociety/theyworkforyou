1934b64cb7ec40118499491afcf9ebbc4bb0e7f1
Struan Donald <struan@exo.org.uk>
2025-09-17 17:19:24 +0100

    update section page to allow adding section level comments
    If an addcomment param is used then it will instead display the comment
    from rather than the section contents.

--- www/includes/easyparliament/templates/html/section/section.php
+++ www/includes/easyparliament/templates/html/section/section.php
@@ -28,7 +28,20 @@ printf(gettext('on <a href="%s">%s</a>'), $debate_day_link, $debate_day_human);
 include '_section_nav.php'; ?>
 </div>
 
-<?php include '_section_comments.php'; ?>
+<?php
+if (isset($show_comment_form)) { ?>
+<div class="full-page">
+    <div class="full-page__row">
+<?php
+    global $PAGE;
+
+    $comments['object']->display('ep', $comments['args']);
+    $PAGE->comment_form($comments['commentdata']);
+?>
+    </div>
+</div>
+<?php } else {
+include '_section_comments.php'; ?>
 
 <div class="full-page">
 
@@ -39,5 +52,6 @@ include '_section_content.php';
 ?>
 
 </div>
+<?php } ?>
 
 <?php include '_section_footer.php'; ?>
