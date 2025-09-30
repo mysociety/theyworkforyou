1cf348a4e60f2e484c5fca0628681b8c789cf186
Struan Donald <struan@exo.org.uk>
2025-09-30 17:24:00 +0100

    editing interface for glossary entries

---
+++ www/includes/easyparliament/templates/html/glossary/editterm_success.php
@@ -0,0 +1,15 @@
+<div class="full-page legacy-page static-page">
+  <div class="full-page__row">
+    <div class="panel">
+      <div class="stripe-side">
+        <div class="main">
+          <h4>Updated</h4>
+  
+          <p>
+            The entry for <a href="<?= $entry_url ?>"><strong>&quot;<?= $title ?>&quot;</strong></a> has been updated.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
