9dfb789506cdf5612bc22a38c098f7c3de43e601
Struan Donald <struan@exo.org.uk>
2025-10-15 14:33:30 +0100

    update glossary form with some hints on markdown

--- www/includes/easyparliament/templates/html/glossary/_editterm_form.php
+++ www/includes/easyparliament/templates/html/glossary/_editterm_form.php
@@ -6,5 +6,5 @@
 
       <p><input type="submit" name="previewterm" value="Preview" class="submit">
       <input type="submit" name="submitterm" value="Post" class="submit"></p></label>
-      <p><small>Markdown formatting is allowed. URLs and email addresses will automatically be turned into links.</small></p>
+      <p><small><a href="https://www.markdownguide.org/cheat-sheet/">Markdown</a> permitted. Try to avoid major headings as it's bad for accessibility. URLs and email addresses will automatically be turned into links.</small></p>
   </div>
