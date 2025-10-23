2ad9944f84214c653220fd73dc01605713251e93
Alex Parsons <alex.parsons@mysociety.org>
2025-10-23 17:16:19 +0100

    Show permission error for glossary pages

--- www/docs/addterm/index.php
+++ www/docs/addterm/index.php
@@ -6,6 +6,13 @@ include_once INCLUDESPATH . "easyparliament/glossarylist.php";
 
 $view = new MySociety\TheyWorkForYou\GlossaryView\AddTermView();
 $data = $view->display();
+
+// Check for permission errors
+if (isset($data['error']) && !isset($data['template_name'])) {
+    $PAGE->error_message($data['error'], true);
+    exit;
+}
+
 $data['PAGE'] = $PAGE;
 
 $template = "glossary/" . $data['template_name'];
