e97093abdba20eb68ede31fec43b35ae8b708282
Alex Parsons <alex.parsons@mysociety.org>
2026-08-10 15:00:05 +0000

    Include SP and Senedd cross-party groups in APPG data

--- classes/DataClass/APPGs/APPGDetails.php
+++ classes/DataClass/APPGs/APPGDetails.php
@@ -14,9 +14,28 @@ use MySociety\TheyWorkForYou\DataClass\BaseModel;
 
 class APPGDetails extends BaseModel {
     public string $slug;
+    public string $parliament = "uk";
     public string $title;
     public string $purpose;
     public ?string $website;
     public string $source_url;
     // public Array $categories;
+
+    /**
+     * Remove the repeated group-type prefix so cards emphasize the unique name.
+     */
+    public function shortTitle(): string {
+        $prefixes = [
+            '/^All-Party Parliamentary Group (on |for )?/i',
+            '/^Cross-Party Group (on |for )?/i',
+            '/^All-Party Group (on |for )?/i',
+        ];
+        foreach ($prefixes as $prefix) {
+            $short = preg_replace($prefix, '', $this->title, count: $count);
+            if ($count) {
+                return ucfirst($short);
+            }
+        }
+        return ucfirst($this->title);
+    }
 }
